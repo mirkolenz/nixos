5a75593cb077770c4a80bbc7ec4757c985a54a0e
Mirko Lenz <mirko@mirkolenz.com>
2023-04-18 22:01:34 +0200

    fix: increase sd tmp file size due to build errors

--- installer/sd.nix
+++ installer/sd.nix
@@ -2,4 +2,9 @@
 {
   imports = [ ./base.nix ];
   sdImage.compressImage = false;
+  boot = {
+    tmpOnTmpfs = true;
+    # default is 50% of RAM, but builds fail with 8G Raspi 4
+    tmpOnTmpfsSize = "16G";
+  };
 }
