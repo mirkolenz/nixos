caf0bf47460243bb1aaa5426b7d3f91042499e19
Mirko Lenz <mirko@mirkolenz.com>
2023-04-18 12:06:12 +0200

    fix: add fish functions

--- users/home/mlenz.nix
+++ users/home/mlenz.nix
@@ -259,7 +259,57 @@ in {
           end
         end
       '';
+      interactiveShellInit = ''
+        # Disable greeting
+        set fish_greeting ""
+      '';
       functions = {
+        dockerup = {
+          body = ''
+            sudo docker compose pull $argv
+            sudo docker compose build $argv
+            sudo docker compose up --detach
+            sudo docker image prune --all --force
+          '';
+          description = "Update all docker-compose containers";
+        };
+        docker-reset = {
+          body = ''
+            sudo docker system prune --all --force
+          '';
+          description = "Reset docker";
+        };
+        encrypt = {
+          description = "Encrypt a file using gpg";
+          body = ''
+            gpg --output "$target" --encrypt --recipient "$recipient" "$source"
+          '';
+          argumentNames = [ "source" "target" "recipient" ];
+        };
+        decrypt = {
+          description = "Decrypt a file using gpg";
+          body = ''
+            argparse source target -- $argv
+            or return
+            gpg --output $argv[2] --decrypt $argv[1]
+          '';
+        };
+        backup = {
+          description = "Backup a directory to a tar.gz file";
+          body = ''
+            mkdir -p "$target"
+            sudo tar czf "$target/$(date +"%Y-%m-%d-%H-%M-%S").tgz" "$source"
+          '';
+          argumentNames = [ "source" "target" ];
+        };
+        restore = {
+          description = "Backup a directory to a tar.gz file";
+          body = ''
+            mkdir -p "$target"
+            sudo tar xf "$source" -C "$target"
+          '';
+          argumentNames = [ "source" "target" ];
+        };
         divider = {
           body = ''
             set length (tput cols)
@@ -275,7 +325,9 @@ in {
           description = "Print a bold horizontal line";
         };
         otf2ttf = {
-          body = "fontforge -c 'Open(\"$source\"); Generate(\"$target\")'";
+          body = ''
+            fontforge -c 'Open("$source"); Generate("$target")'
+          '';
           description = "Convert an OpenType font to TrueType";
           wraps = "fontforge";
           argumentNames = [ "source" "target" ];
