d1f13a6ca2c72f8e7dd7e5abb3a4b859e7904c7c
Mirko Lenz <mirko@mirkolenz.com>
2023-04-19 21:58:06 +0200

    fix: raspi installer not working due to bug

--- installer/raspi.nix
+++ installer/raspi.nix
@@ -4,4 +4,11 @@
     ./sd.nix
     ../hosts/raspi/hotfix.nix
   ];
+  # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
 }
