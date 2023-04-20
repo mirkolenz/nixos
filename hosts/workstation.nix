c1915e18a6c681233d8135f5c213b7f7ac44b311
Mirko Lenz <mirko@mirkolenz.com>
2023-04-20 21:25:10 +0200

    feat: use kde plasma

--- hosts/workstation.nix
+++ hosts/workstation.nix
@@ -8,8 +8,6 @@
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
     layout = "us";
     xkbVariant = "";
     excludePackages = with pkgs; [
@@ -19,11 +17,21 @@
     displayManager.autoLogin.enable = true;
     displayManager.autoLogin.user = "mlenz";
   };
+  # KDE Plasma 5
+  services.xserver = {
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+  };
+  # GNOME
+  # services.xserver = {
+  #   displayManager.gdm.enable = true;
+  #   desktopManager.gnome.enable = true;
+  # };
   # Remove default GNOME apps.
-  services.gnome.core-utilities.enable = false;
+  # services.gnome.core-utilities.enable = false;
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  # systemd.services."getty@tty1".enable = false;
+  # systemd.services."autovt@tty1".enable = false;
   # Printing
   services.printing.enable = true;
   # Enable sound with pipewire.
