251cb8342c32c3ef82a8c3d485cfd7a52d3d1483
Mirko Lenz <mirko@mirkolenz.com>
2023-04-22 23:54:43 +0200

    fix: enable nix store optimizations and gc

--- system/common/default.nix
+++ system/common/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, extras, ... }:
+{ pkgs, extras, lib, ... }:
 let
   inherit (extras) unstable;
 in
@@ -22,5 +22,20 @@ in
 
   nix = {
     package = pkgs.nix;
+    settings = {
+      auto-optimise-store = true;
+    };
+    gc = lib.mkMerge [
+      {
+        automatic = true;
+        options = "--delete-older-than 30d";
+      }
+      (lib.optionalAttrs (pkgs.stdenv.isLinux) {
+        dates = "weekly";
+      })
+      (lib.optionalAttrs (pkgs.stdenv.isDarwin) {
+        interval = { Weekday = 0; Hour = 0; Minute = 0; };
+      })
+    ];
   };
 }
