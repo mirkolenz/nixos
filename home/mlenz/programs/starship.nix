30208aa79a484a26c747b69d3b3c5d69960c289e
Mirko Lenz <mirko@mirkolenz.com>
2023-04-23 21:55:36 +0200

    fix: update starship characters

--- home/mlenz/programs/starship.nix
+++ home/mlenz/programs/starship.nix
@@ -2,5 +2,12 @@
 {
   programs.starship = {
     enable = true;
+    settings = {
+      add_newline = true;
+      character = {
+        success_symbol = '[❯](bold green)';
+        error_symbol = '[🗙](bold red)';
+      };
+    };
   };
 }
