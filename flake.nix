d983f067d0424d33f5993a19b2aa9dca0a003916
Mirko Lenz <mirko@mirkolenz.com>
2023-04-23 10:04:25 +0200

    refactor: use flake-parts to merge devshell

--- flake.nix
+++ flake.nix
@@ -16,8 +16,8 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
     };
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
@@ -44,110 +44,134 @@
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, darwin, nixos-generators, vscode-server, nixpkgs-unstable, nixos-hardware, mlenz-ssh-keys, ... }:
-  let
-    defaults = { pkgs, ... }: {
-      nixpkgs.config.allowUnfree = true;
-      # https://github.com/nix-community/home-manager/issues/1538
-      _module.args = {
-        extras = {
-          inherit inputs;
-          unstable = import nixpkgs-unstable {
-            inherit (pkgs.stdenv.targetPlatform) system;
-            config.allowUnfree = true;
+  outputs =
+    inputs@{ darwin
+    , flake-parts
+    , home-manager
+    , nixos-generators
+    , nixos-hardware
+    , nixpkgs-unstable
+    , nixpkgs
+    , vscode-server
+    , ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { config, pkgs, system, ... }: {
+        devShells.default = pkgs {
+          packages = with pkgs; [
+            nixpkgs-fmt
+            nil
+            rnix-lsp
+            git
+          ];
+        };
+      };
+      flake =
+        let
+          defaults = { pkgs, ... }: {
+            nixpkgs.config.allowUnfree = true;
+            # https://github.com/nix-community/home-manager/issues/1538
+            _module.args = {
+              extras = {
+                inherit inputs;
+                unstable = import nixpkgs-unstable {
+                  inherit (pkgs.stdenv.targetPlatform) system;
+                  config.allowUnfree = true;
+                };
+                dummyPackage = (pkgs.writeShellScriptBin "dummy" ":");
+                username = "mlenz";
+                stateVersion = "22.11";
+              };
+            };
           };
-          dummyPackage = (pkgs.writeShellScriptBin "dummy" ":");
-          username = "mlenz";
-          stateVersion = "22.11";
+        in
+        {
+          packages = {
+            aarch64-linux = {
+              raspi = nixos-generators.nixosGenerate {
+                system = "aarch64-linux";
+                # customFormats = {
+                #   "myFormat" = {
+                #     imports = [
+                #       "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+                #     ];
+                #     formatAttr = "sdImage";
+                #   };
+                # };
+                format = "sd-aarch64-installer";
+                modules = [
+                  defaults
+                  ./installer/raspi.nix
+                ];
+              };
+            };
+            x86_64-linux = {
+              iso = nixos-generators.nixosGenerate {
+                system = "x86_64-linux";
+                format = "install-iso";
+                modules = [
+                  defaults
+                  ./installer/iso.nix
+                ];
+              };
+            };
+          };
+          nixosConfigurations = {
+            vm = nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              modules = [
+                defaults
+                home-manager.nixosModules.home-manager
+                vscode-server.nixosModule
+                ./hosts/vm
+              ];
+            };
+            homeserver = nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              modules = [
+                defaults
+                home-manager.nixosModules.home-manager
+                nixos-hardware.nixosModules.common-pc-ssd
+                nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+                nixos-hardware.nixosModules.common-gpu-amd
+                nixos-hardware.nixosModules.common-hidpi
+                ./hosts/homeserver
+              ];
+            };
+            raspi = nixpkgs.lib.nixosSystem {
+              system = "aarch64-linux";
+              modules = [
+                defaults
+                home-manager.nixosModules.home-manager
+                nixos-hardware.nixosModules.raspberry-pi-4
+                ./hosts/raspi
+              ];
+            };
+            macbook-nixos = nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              modules = [
+                defaults
+                home-manager.nixosModules.home-manager
+                nixos-hardware.nixosModules.common-pc-laptop-ssd
+                nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+                nixos-hardware.nixosModules.common-gpu-intel
+                nixos-hardware.nixosModules.common-gpu-nvidia-disable
+                nixos-hardware.nixosModules.common-hidpi
+                ./hosts/macbook-nixos
+              ];
+            };
+          };
+          darwinConfigurations = {
+            macbook = darwin.lib.darwinSystem {
+              system = "x86_64-darwin";
+              modules = [
+                defaults
+                home-manager.darwinModules.home-manager
+                ./hosts/macbook
+              ];
+            };
+          };
         };
-      };
     };
-  in
-  {
-    packages = {
-      aarch64-linux = {
-        raspi = nixos-generators.nixosGenerate {
-          system = "aarch64-linux";
-          # customFormats = {
-          #   "myFormat" = {
-          #     imports = [
-          #       "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-          #     ];
-          #     formatAttr = "sdImage";
-          #   };
-          # };
-          format = "sd-aarch64-installer";
-          modules = [
-            defaults
-            ./installer/raspi.nix
-          ];
-        };
-      };
-      x86_64-linux = {
-        iso = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          format = "install-iso";
-          modules = [
-            defaults
-            ./installer/iso.nix
-          ];
-        };
-      };
-    };
-    nixosConfigurations = {
-      vm = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          defaults
-          home-manager.nixosModules.home-manager
-          vscode-server.nixosModule
-          ./hosts/vm
-        ];
-      };
-      homeserver = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          defaults
-          home-manager.nixosModules.home-manager
-          nixos-hardware.nixosModules.common-pc-ssd
-          nixos-hardware.nixosModules.common-cpu-intel-cpu-only
-          nixos-hardware.nixosModules.common-gpu-amd
-          nixos-hardware.nixosModules.common-hidpi
-          ./hosts/homeserver
-        ];
-      };
-      raspi = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          defaults
-          home-manager.nixosModules.home-manager
-          nixos-hardware.nixosModules.raspberry-pi-4
-          ./hosts/raspi
-        ];
-      };
-      macbook-nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          defaults
-          home-manager.nixosModules.home-manager
-          nixos-hardware.nixosModules.common-pc-laptop-ssd
-          nixos-hardware.nixosModules.common-cpu-intel-cpu-only
-          nixos-hardware.nixosModules.common-gpu-intel
-          nixos-hardware.nixosModules.common-gpu-nvidia-disable
-          nixos-hardware.nixosModules.common-hidpi
-          ./hosts/macbook-nixos
-        ];
-      };
-    };
-    darwinConfigurations = {
-      macbook = darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
-        modules = [
-          defaults
-          home-manager.darwinModules.home-manager
-          ./hosts/macbook
-        ];
-      };
-    };
-  };
 }
--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    nixpkgs-fmt
-    nil
-    rnix-lsp
-    git
-  ];
-}
