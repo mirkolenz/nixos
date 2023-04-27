dc3b76d8d4212837a48de93095559de977abc2c0
Mirko Lenz <mirko@mirkolenz.com>
2023-04-27 13:57:31 +0200

    feat: activate homebrew config

--- system/darwin/homebrew.nix
+++ system/darwin/homebrew.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, config, ... }:
 {
   homebrew = {
-    enable = false; # TODO
+    enable = true;
     global = {
       autoUpdate = true;
     };
@@ -23,67 +23,66 @@
     ];
     casks = [
       "1password"
-      "adobe-creative-cloud"
-      "anydesk"
+      # "adobe-creative-cloud"
+      # "anydesk"
       "appcleaner"
       "arc"
-      "arq"
+      # todo: "arq"
       "balenaetcher"
-      "banking-4"
+      # "banking-4"
       "bartender"
-      "cleanshot"
-      "contexts"
-      "default-folder-x"
+      # "cleanshot"
+      # "contexts"
+      # "default-folder-x"
       "devonthink"
       "discord"
-      "figma"
-      "firefox"
-      "fission"
+      # "figma"
+      # "firefox"
+      # "fission"
       "fork"
-      "gobdokumente"
-      "google-chrome"
-      "handbrake"
-      "hook"
-      "iina"
+      # "gobdokumente"
+      # "handbrake"
+      # "hook"
+      # "iina"
       "iterm2"
-      "kaleidoscope"
-      "kindle"
-      "mediathekview"
-      "mullvadvpn"
+      # "kaleidoscope"
+      # "kindle"
+      # "mediathekview"
+      # "mullvadvpn"
       "notion"
       "obsidian"
-      "omnigraffle"
+      # "omnigraffle"
       "openinterminal"
-      "orbstack"
+      # "orbstack"
       "orion"
       "parallels"
-      "pixelsnap"
-      "postico"
-      "postman"
+      # "pixelsnap"
+      # "postico"
+      # "postman"
       "presentation"
       "raindropio"
-      "raspberry-pi-imager"
+      # "raspberry-pi-imager"
       "raycast"
-      "rectangle-process-manager"
-      "replacicon"
-      "shortcat"
-      "soundsource"
+      "rectangle-pro"
+      # "replacicon"
+      # "shortcat"
+      # "soundsource"
       "steermouse"
       "tiptoi-manager"
-      "utm"
+      # "utm"
       "viscosity"
       "visual-studio-code"
       "zoom"
-      "zotero"
+      # "zotero"
       # Fonts
       "font-eb-garamond"
       "font-fira-code"
       "font-fira-mono"
       "font-fira-sans"
-      "font-firacode-nerd-font"
+      "font-fira-code-nerd-font"
       "font-fontawesome"
       "font-ia-writer-duo"
-      "font-ia-writer-duospace"
+      # todo: fails to install "font-ia-writer-duospace"
       "font-ia-writer-mono"
       "font-ia-writer-quattro"
       "font-ibm-plex"
@@ -122,56 +121,51 @@
       "font-tex-gyre-termes"
       "font-tex-gyre-termes-math"
       "font-ubuntu"
+      "font-ubuntu-nerd-font"
+      "font-ubuntu-mono"
+      "font-ubuntu-mono-nerd-font"
       "font-varela-round"
     ];
     masApps = {
       "1Password for Safari" = 1569813296;
       "AusweisApp2" = 948660805;
-      "Baby Monitor" = 517602535;
       "Bitwarden" = 1352778147;
-      "CotEditor" = 1024640650;
-      "DaisyDisk" = 411643860;
-      "Diagrams" = 1276248849;
       "EasyLetter" = 1495179755;
-      "Final Cut Pro" = 424389933;
       "Gapplin" = 768053424;
-      "GoodNotes" = 1444383602;
       "Home Assistant" = 1099568401;
-      "Image2icon" = 992115977;
-      "Infuse" = 1136220934;
-      "iStat Menus" = 1319778037;
+      # "iStat Menus" = 1319778037;
       "Kagi Inc." = 1622835804;
       "Keka" = 470158793;
-      "Live Home 3D Pro" = 1066145115;
-      "MakePass" = 1450989464;
+      # "Live Home 3D Pro" = 1066145115;
+      # "MakePass" = 1450989464;
       "Mela" = 1568924476;
-      "Messenger" = 1480068668;
+      # "Messenger" = 1480068668;
       "Microsoft Excel" = 462058435;
       "Microsoft Outlook" = 985367838;
       "Microsoft PowerPoint" = 462062816;
       "Microsoft Word" = 462054704;
-      "Minimal Twitter" = 1668204600;
+      # "Minimal Twitter" = 1668204600;
       "OneDrive" = 823766827;
-      "OwlOCR" = 1499181666;
-      "Pause" = 1599313358;
+      # "OwlOCR" = 1499181666;
+      # "Pause" = 1599313358;
       "PDF Expert" = 1055273043;
-      "PDFZone" = 1215383084;
-      "Pixelmator Pro" = 1289583905;
+      # "PDFZone" = 1215383084;
+      # "Pixelmator Pro" = 1289583905;
       "Prime Video" = 545519333;
       "Pure Paste" = 1611378436;
       "Reeder" = 1529448980;
-      "Refined GitHub" = 1519867270;
+      # "Refined GitHub" = 1519867270;
       "Save to Raindrop.io" = 1549370672;
       "ShellFish" = 1336634154;
-      "SiteSucker" = 442168834;
+      # "SiteSucker" = 442168834;
       "Step Two" = 1448916662;
       "StopTheMadness" = 1376402589;
       "Tempus" = 1491326665;
-      "TestFlight" = 899247664;
+      # todo: fails to install "TestFlight" = 899247664;
       "Todoist" = 585829637;
-      "Velja" = 1607635845;
+      # "Velja" = 1607635845;
       "Vinegar" = 1591303229;
-      "WireGuard" = 1451685025;
+      # "WireGuard" = 1451685025;
       "Yoink" = 457622435;
     };
     whalebrews = [ ];
