3aa4769d322f62d2b0b5a46719cb48bfec6a55e8
Mirko Lenz <mirko@mirkolenz.com>
2023-04-27 13:57:57 +0200

    fix: use drive labels for nixos macbook

--- hosts/macbook-nixos/hardware.nix
+++ hosts/macbook-nixos/hardware.nix
@@ -8,16 +8,16 @@
   boot.kernelModules = [ "kvm-intel" "wl" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/f1c8b33e-bfdc-48d7-84cd-4a7c230b2a7a";
+    device = "/dev/disk/by-label/root";
     fsType = "ext4";
   };
   fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-uuid/D1BB-770E";
+    device = "/dev/disk/by-label/boot";
     fsType = "vfat";
   };
   swapDevices = [
     {
-      device = "/dev/disk/by-uuid/5f7a5672-22ec-4318-869c-379c44c4571b";
+      device = "/dev/disk/by-label/swap";
     }
   ];
   networking.useDHCP = lib.mkDefault true;
