e28de321081f4c45faed76fc63e3fa2dc105d2a3
Mirko Lenz <mirko@mirkolenz.com>
2023-05-09 08:50:23 +0200

    feat(ssh): add raspi host, remove id_rsa from keys

--- home/mlenz/programs/ssh.nix
+++ home/mlenz/programs/ssh.nix
@@ -8,7 +8,7 @@
           UseKeychain = "yes";
           AddKeysToAgent = "yes";
         };
-        identityFile = [ "id_ed25519" "id_rsa" ];
+        identityFile = [ "id_ed25519" ];
       };
       wi2gpu = {
         hostname = "136.199.130.136";
@@ -22,6 +22,11 @@
       };
       homeserver = {
         hostname = "10.16.2.22";
+        forwardAgent = true;
+        user = "mlenz";
+      };
+      raspi = {
+        hostname = "10.16.2.23";
         forwardAgent = true;
         user = "mlenz";
       };
