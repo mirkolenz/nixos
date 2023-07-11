ec5fae9821b744e272eee10d7e879ed5e7e27d6b
Mirko Lenz <mirko@mirkolenz.com>
2023-07-11 09:55:11 +0200

    fix(darwin): make builder customizable

--- system/darwin/builder.nix
+++ system/darwin/builder.nix
@@ -1,13 +1,31 @@
-{...}: {
-  nix.linux-builder = {
-    enable = false;
-    modules = [
-      # {
-      #   virtualisation.darwin-builder = {
-      #     diskSize = 5120;
-      #     memorySize = 1024;
-      #   };
-      # }
-    ];
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.custom.builder;
+in {
+  options.custom.builder = {
+    enable = lib.mkOption {
+      default = false;
+      example = false;
+      description = "Enable the Darwin linux builder";
+      type = lib.types.bool;
+    };
+    uncachedHardware = lib.mkEnableOption "Use better hardware for vm that is not cached by hydra";
+  };
+  config = lib.mkIf cfg.enable {
+    nix.linux-builder = {
+      enable = true;
+      modules = lib.mkIf cfg.uncachedHardware [
+        {
+          # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/nixos/modules/profiles/macos-builder.nix
+          virtualisation.darwin-builder = {
+            diskSize = 20 * 1024;
+            memorySize = 6 * 1024;
+          };
+        }
+      ];
+    };
   };
 }
