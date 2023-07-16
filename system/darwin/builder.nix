979c588221e325296a7d48b205cd048c5c2a387a
Mirko Lenz <mirko@mirkolenz.com>
2023-07-16 10:57:17 +0100

    fix(darwin): remove custom modules from builder

--- system/darwin/builder.nix
+++ system/darwin/builder.nix
@@ -1,31 +1,12 @@
-{
-  config,
-  lib,
-  ...
-}: let
-  cfg = config.custom.builder;
-in {
-  options.custom.builder = {
-    enable = lib.mkOption {
-      default = false;
-      example = false;
-      description = "Enable the Darwin linux builder";
-      type = lib.types.bool;
-    };
-    uncachedHardware = lib.mkEnableOption "Use better hardware for vm that is not cached by hydra";
-  };
-  config = lib.mkIf cfg.enable {
-    nix.linux-builder = {
-      enable = true;
-      modules = lib.mkIf cfg.uncachedHardware [
-        {
-          # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/nixos/modules/profiles/macos-builder.nix
-          virtualisation.darwin-builder = {
-            diskSize = 20 * 1024;
-            memorySize = 6 * 1024;
-          };
-        }
-      ];
-    };
+{...}: {
+  nix.linux-builder = {
+    enable = false;
+    modules = [
+      # swraid accesses system.stateVersion which throws an error in the builder
+      # https://github.com/NixOS/nixpkgs/pull/183314/files
+      # {
+      #   boot.swraid.enable = false;
+      # }
+    ];
   };
 }
