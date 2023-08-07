8abaf2e81ffae92805a2458277b51552aaa8176b
Mirko Lenz <mirko@mirkolenz.com>
2023-08-07 10:13:26 +0200

    feat: add importFolder function

--- flake.nix
+++ flake.nix
@@ -103,7 +103,11 @@
     systems,
     ...
   }: let
-    defaults = {pkgs, ...}: {
+    defaults = {
+      pkgs,
+      lib,
+      ...
+    }: {
       imports = [
         ./overlays
       ];
@@ -127,6 +131,11 @@
         extras = {
           dummyPackage = pkgs.writeShellScriptBin "dummy" ":";
           stateVersion = "23.05";
+          importFolder = folder: let
+            toImport = name: value: folder + ("/" + name);
+            filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key && key != "default.nix";
+          in
+            lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
         };
       };
     };
