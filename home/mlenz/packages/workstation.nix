a6ab616fde44febd2a079a8af4443e1817aa7eca
Mirko Lenz <mirko@mirkolenz.com>
2023-09-07 10:03:35 +0200

    fix: use poetry from stable channel

--- home/mlenz/packages/workstation.nix
+++ home/mlenz/packages/workstation.nix
@@ -1,11 +1,12 @@
 {
   pkgs,
+  pkgsStable,
   lib,
   osConfig,
   ...
 }: let
   # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/tools/package-management/poetry/default.nix#L40
-  poetry = pkgs.poetry.withPlugins (ps: with ps; [poetry-plugin-up]);
+  poetry = pkgsStable.poetry.withPlugins (ps: with ps; [poetry-plugin-up]);
 in
   lib.mkIf (pkgs.stdenv.isDarwin || (lib.attrByPath ["services" "xserver" "enable"] true osConfig)) {
     home.packages = with pkgs; [
