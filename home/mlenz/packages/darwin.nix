504320b4a7f0b400aeb9f0fc2c766498422eebca
Mirko Lenz <mirko@mirkolenz.com>
2023-11-02 09:36:07 +0100

    fix(darwin): re-enable bibtex2cff

--- home/mlenz/packages/darwin.nix
+++ home/mlenz/packages/darwin.nix
@@ -9,9 +9,7 @@ lib.mkIf pkgs.stdenv.isDarwin {
     pkgsStable.texlive.combined.scheme-full
     tectonic
     texlab
-    # TODO: Causes signal SIGSEGV (Address boundary error)
-    # https://github.com/nix-community/poetry2nix/issues/1291
-    # bibtex2cff
+    bibtex2cff
     bibtex-to-cff
   ];
 }
