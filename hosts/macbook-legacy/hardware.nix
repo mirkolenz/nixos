ad9e08e1e22ea35eb3b13c85b9a51cf02bfcf6bd
Mirko Lenz <mirko@mirkolenz.com>
2023-11-18 11:34:16 +0100

    fix: use correct filesystems for macbook-legacy

--- hosts/macbook-legacy/hardware.nix
+++ hosts/macbook-legacy/hardware.nix
@@ -16,7 +16,7 @@
     device = "/dev/disk/by-label/root";
     fsType = "ext4";
   };
-  fileSystems."/boot/efi" = {
+  fileSystems."/boot" = {
     device = "/dev/disk/by-label/boot";
     fsType = "vfat";
   };
