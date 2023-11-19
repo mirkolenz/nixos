ee84757a06fff1e46a0f277fa990547db4750287
Mirko Lenz <mirko@mirkolenz.com>
2023-11-19 21:10:55 +0100

    fix: syntax error

--- system/common/ssh.nix
+++ system/common/ssh.nix
@@ -1,4 +1,4 @@
-{}: {
+{...}: {
   programs.ssh.knownHosts = {
     "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
