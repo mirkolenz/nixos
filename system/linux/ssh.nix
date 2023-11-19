6efc5abc65535311390eff7ef4e95f5d222a75fb
Mirko Lenz <mirko@mirkolenz.com>
2023-11-19 21:28:27 +0100

    feat(linux): specify ssh host keys

--- system/linux/ssh.nix
+++ system/linux/ssh.nix
@@ -1,9 +1,17 @@
 {inputs, ...}: {
-  services.openssh.settings = {
-    PasswordAuthentication = false;
-    KbdInteractiveAuthentication = false;
-    PermitRootLogin = "no";
-    X11Forwarding = false;
+  services.openssh = {
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      X11Forwarding = false;
+    };
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
   # https://discourse.nixos.org/t/fetching-ssh-public-keys/12076
   users.users.mlenz = {
