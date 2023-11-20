cd4d2bb6d637720e9b9f8989de0afd2288b3243d
Mirko Lenz <mirko@mirkolenz.com>
2023-11-20 21:24:31 +0100

    feat: add command for cleaning bibtex files

--- home/mlenz/packages/darwin.nix
+++ home/mlenz/packages/darwin.nix
@@ -6,22 +6,46 @@
 }:
 lib.mkIf pkgs.stdenv.isDarwin {
   home.packages = with pkgs; [
+    mas
     texliveFull
     stable.tectonic
     texlab
     bibtex2cff
     bibtexbrowser2cff
-    mas
+    bibtex-tidy
+    (pkgs.writeShellApplication {
+      name = "bibtidy";
+      text = let
+        maxAuthors = 10;
+        omitFields = [
+          "abstract"
+          "address"
+          "annotation"
+          "archiveprefix"
+          "doi"
+          "editor"
+          "eprint"
+          "isbn"
+          "issn"
+          "primaryclass"
+          "url"
+          "urldate"
+        ];
+      in ''
+        ${lib.getExe pkgs.bibtex-tidy} --v2 \
+          --no-align --no-wrap --blank-lines --no-escape \
+          --omit="${lib.concatStringsSep "," omitFields}" \
+          --max-authors="${builtins.toString maxAuthors}" \
+          "$@"
+      '';
+    })
     (pkgs.writeShellApplication {
       name = "bibcopy";
       text = ''
-        if [ "$#" -lt 1 ]; then
-          echo "Usage: $0 FORMAT (biblatex/bibtex) [SOURCE_DIR] [TARGET_DIR]" >&2
-          exit 1
-        fi
+        format="''${1:-bibtex}"
         sourceDir="''${2:-${config.home.homeDirectory}/Developer/mirkolenz/bibliography}"
         targetDir="''${3:-.}"
-        cp -f "$sourceDir/$1.bib" "$targetDir/references.bib"
+        bibtidy --output="$targetDir/references.bib" "$sourceDir/$format.bib"
         cp -f "$sourceDir/acronyms.tex" "$targetDir/acronyms.tex"
       '';
     })
