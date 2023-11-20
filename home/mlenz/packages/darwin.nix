9b85715c946a3b07256c4b8deeb2dc66d2029129
Mirko Lenz <mirko@mirkolenz.com>
2023-11-20 12:28:20 +0100

    feat(darwin): add bibtex-related commands

--- home/mlenz/packages/darwin.nix
+++ home/mlenz/packages/darwin.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }:
 lib.mkIf pkgs.stdenv.isDarwin {
@@ -11,5 +12,21 @@ lib.mkIf pkgs.stdenv.isDarwin {
     bibtex2cff
     bibtex-to-cff
     mas
+    (pkgs.writeShellApplication {
+      name = "bibcopy";
+      text = ''
+        if [ "$#" -ne 1 ]; then
+          echo "Usage: $0 FORMAT (biblatex or bibtex)" >&2
+          exit 1
+        fi
+        exec cp -f "${config.home.homeDirectory}/Developer/mirkolenz/bibliography/references-$1.bib" "references.bib"
+      '';
+    })
+    (pkgs.writeShellApplication {
+      name = "acrocopy";
+      text = ''
+        exec cp -f "${config.home.homeDirectory}/Developer/mirkolenz/bibliography/acro.tex" "acro.tex"
+      '';
+    })
   ];
 }
