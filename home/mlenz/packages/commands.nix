de72e37a91bf1a20cfa5f517b0aacc05ad791198
Mirko Lenz <mirko@mirkolenz.com>
2023-11-30 22:12:01 +0100

    refactor: apply lib.getExe' treewide

--- home/mlenz/packages/commands.nix
+++ home/mlenz/packages/commands.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: let
-  echo = "${lib.getBin pkgs.coreutils}/bin/echo";
+  echo = lib.getExe' pkgs.coreutils "echo";
   checkSudo = inputs.self.lib.checkSudo;
   cmdTexts = {
     pull-rebuild = ''
@@ -23,7 +23,7 @@
         ${echo} "Usage: $0 INPUT_FILE OUTPUT_FOLDER QUALITY" >&2
         exit 1
       fi
-      exec ${lib.getBin pkgs.imagemagick}/bin/mogrify -path "$2" -strip -interlace none -sampling-factor 4:2:0 -define jpeg:dct-method=float -quality "$3" "$1"
+      exec ${lib.getExe' pkgs.imagemagick "mogrify"} -path "$2" -strip -interlace none -sampling-factor 4:2:0 -define jpeg:dct-method=float -quality "$3" "$1"
     '';
     # https://masdilor.github.io/use-imagemagick-to-resize-and-compress-images/
     mogrify-resize = ''
@@ -31,20 +31,20 @@
         ${echo} "Usage: $0 INPUT_FILE OUTPUT_FOLDER QUALITY FINAL_SIZE" >&2
         exit 1
       fi
-      exec ${lib.getBin pkgs.imagemagick}/bin/mogrify -path "$2" -filter Triangle -define filter:support=2 -thumbnail "$4" -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality "$3" -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB "$1"
+      exec ${lib.getExe' pkgs.imagemagick "mogrify"} -path "$2" -filter Triangle -define filter:support=2 -thumbnail "$4" -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality "$3" -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB "$1"
     '';
     gc = ''
       ${checkSudo}
       set -x #echo on
-      "$SUDO" ${lib.getBin pkgs.nix}/bin/nix-collect-garbage --delete-older-than 7d
-      ${lib.getBin pkgs.nix}/bin/nix-collect-garbage --delete-older-than 7d
+      "$SUDO" ${lib.getExe' pkgs.nix "nix-collect-garbage"} --delete-older-than 7d
+      ${lib.getExe' pkgs.nix "nix-collect-garbage"} --delete-older-than 7d
       ${lib.getExe pkgs.nix} store gc
       ${lib.getExe pkgs.nix} store optimise
     '';
     # https://github.com/NixOS/nixpkgs/blob/nixos-23.11/nixos/modules/tasks/auto-upgrade.nix#L204
     needs-reboot = ''
-      booted="$(${lib.getBin pkgs.coreutils}/bin/readlink /run/booted-system/{initrd,kernel,kernel-modules})"
-      built="$(${lib.getBin pkgs.coreutils}/bin/readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
+      booted="$(${lib.getExe' pkgs.coreutils "readlink"} /run/booted-system/{initrd,kernel,kernel-modules})"
+      built="$(${lib.getExe' pkgs.coreutils "readlink"} /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
 
       if [ "$booted" != "$built" ]; then
         ${echo} "REBOOT NEEDED"
@@ -99,8 +99,8 @@
         exit 1
       fi
       ${checkSudo}
-      ${lib.getBin pkgs.coreutils}/bin/mkdir -p "$2"
-      TIMESTAMP=$(${lib.getBin pkgs.coreutils}/bin/date +"%Y-%m-%d-%H-%M-%S")
+      ${lib.getExe' pkgs.coreutils "mkdir"} -p "$2"
+      TIMESTAMP=$(${lib.getExe' pkgs.coreutils "date"} +"%Y-%m-%d-%H-%M-%S")
       "$SUDO" ${lib.getExe pkgs.gnutar} czf "$2/$TIMESTAMP.tgz" "$1"
     '';
     restore = ''
@@ -109,15 +109,15 @@
         exit 1
       fi
       ${checkSudo}
-      ${lib.getBin pkgs.coreutils}/bin/mkdir -p "$2"
+      ${lib.getExe' pkgs.coreutils "mkdir"} -p "$2"
       "$SUDO" ${lib.getExe pkgs.gnutar} xf "$1" -C "$2"
     '';
     nixos-env = ''
       ${checkSudo}
-      exec "$SUDO" ${lib.getBin pkgs.nix}/bin/nix-env --profile /nix/var/nix/profiles/system "$@"
+      exec "$SUDO" ${lib.getExe' pkgs.nix "nix-env"} --profile /nix/var/nix/profiles/system "$@"
     '';
     nix-prefetch-sri = ''
-      exec ${lib.getExe pkgs.nix} hash to-sri --type "sha256" "$(${lib.getBin pkgs.nix}/bin/nix-prefetch-url "$@")"
+      exec ${lib.getExe pkgs.nix} hash to-sri --type "sha256" "$(${lib.getExe' pkgs.nix "nix-prefetch-url"} "$@")"
     '';
     nix-prefetch-sri-batch = ''
       if [ "$#" -ne 1 ]; then
@@ -126,7 +126,7 @@
       fi
       exec ${lib.getExe pkgs.nix} eval --raw "$1" \
        --apply "attrs: builtins.concatStringsSep \"\\n\" (builtins.attrValues (builtins.mapAttrs (name: value: value.url) attrs))" \
-       | ${lib.getBin pkgs.findutils}/bin/xargs -l ${lib.getExe cmds.nix-prefetch-sri}
+       | ${lib.getExe' pkgs.findutils "xargs"} -l ${lib.getExe cmds.nix-prefetch-sri}
     '';
   };
   cmds = lib.mapAttrs (name: text: pkgs.writeShellApplication {inherit name text;}) cmdTexts;
