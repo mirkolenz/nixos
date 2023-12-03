7de984db8d81abe8449e91065ae96138464b7da4
Mirko Lenz <mirko@mirkolenz.com>
2023-12-03 03:02:31 +0100

    fix: improve importFolder

--- lib/default.nix
+++ lib/default.nix
@@ -1,10 +1,12 @@
 lib: {
-  # https://github.com/infinisil/system/blob/b8bbfde10411ae7a673ac49c60efce56f3c2bc57/config/new-modules/default.nix
   importFolder = dir: let
     toImport = name: value: dir + ("/" + name);
     filterFiles = name: value:
-      (value == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
-      || value == "directory";
+      !lib.hasPrefix "_" name
+      && (
+        (value == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
+        || value == "directory"
+      );
   in
     lib.mapAttrsToList toImport (lib.filterAttrs filterFiles (builtins.readDir dir));
   checkSudo = ''
