106107db0e06209edb04223ab74e8ff6293e7492
Mirko Lenz <mirko@mirkolenz.com>
2023-12-20 21:45:55 +0100

    feat(oci): add unshare command to wrapper

--- system/linux/oci/wrapper.nix
+++ system/linux/oci/wrapper.nix
@@ -43,6 +43,9 @@
       if [ "$1" = "journal" ]; then
         exec sudo journalctl --unit "podman-$2.service" "''${@:3}"
       fi
+      if [ "$1" = "unshare" ]; then
+        exec unshare --user --map-auto --setuid "$1" --setgid "$2" -- "''${@:2}"
+      fi
     '';
   };
 in {
