f79fc9e3fc223e1d9523e03ee298f94849fe7bfb
Mirko Lenz <mirko@mirkolenz.com>
2023-12-20 22:00:51 +0100

    refactor(oci): update podman exec in wrapper

--- system/linux/oci/wrapper.nix
+++ system/linux/oci/wrapper.nix
@@ -32,10 +32,10 @@
         exit 1
       fi
       if [ "$1" = "run" ]; then
-        exec sudo ${lib.getExe pkgs.podman} run ${lib.escapeShellArgs podmanArgs} "''${@:2}"
+        exec sudo podman run ${lib.escapeShellArgs podmanArgs} "''${@:2}"
       fi
       if [ "$1" = "exec" ]; then
-        exec sudo ${lib.getExe pkgs.podman} exec "''${@:2}"
+        exec sudo podman exec "''${@:2}"
       fi
       if [ "$1" = "service" ]; then
         exec sudo systemctl "''${3:-status}" "podman-$2.service" "''${@:4}"
