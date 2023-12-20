db777c6eea3f5e91f423a6498ccb3fe7e0607d09
Mirko Lenz <mirko@mirkolenz.com>
2023-12-20 20:21:59 +0100

    feat(oci): add more commands to wrapper

--- system/linux/oci/wrapper.nix
+++ system/linux/oci/wrapper.nix
@@ -17,6 +17,34 @@
     ++ (cli.mkHosts {})
     ++ (cli.mkCaps {})
     ++ (cli.mkSysctls {});
+
+  wrapper = pkgs.writeShellApplication {
+    name = "oci";
+    text = ''
+      if [ "$#" -eq 0 ]; then
+        echo "Usage: oci <command> [<args>]"
+        echo ""
+        echo "Available commands:"
+        echo "exec: Run a command in an existing container"
+        echo "run: Run a command in a new container"
+        echo "service: Control the systemd service"
+        echo "journal: Show the logs of the podman service"
+        exit 1
+      fi
+      if [ "$1" -eq "run" ]; then
+        exec sudo ${lib.getExe pkgs.podman} run ${lib.escapeShellArgs podmanArgs} "''${@:2}"
+      fi
+      if [ "$1" -eq "exec" ]; then
+        exec sudo ${lib.getExe pkgs.podman} exec "''${@:2}"
+      fi
+      if [ "$1" -eq "service" ]; then
+        exec sudo systemctl "''${3:-status}" "podman-$2.service" "''${@:}"
+      fi
+      if [ "$1" -eq "journal" ]; then
+        exec sudo journalctl --unit "podman-$2.service" "''${@:3}"
+      fi
+    '';
+  };
 in {
   options.custom.oci.shellWrapper = with lib; {
     enable = mkOption {
@@ -27,12 +55,7 @@ in {
 
   config = lib.mkIf (cfg.enable && wrapperCfg.enable) {
     environment.systemPackages = [
-      (pkgs.writeShellApplication {
-        name = "oci-run";
-        text = ''
-          exec sudo ${lib.getExe pkgs.podman} run ${lib.escapeShellArgs podmanArgs} "$@"
-        '';
-      })
+      wrapper
     ];
   };
 }
