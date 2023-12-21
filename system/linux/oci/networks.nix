c838543430b89309ec46b0dbc6b1df9ce97caa4c
Mirko Lenz <mirko@mirkolenz.com>
2023-12-21 10:55:55 +0100

    fix(oci): add options to network config

--- system/linux/oci/networks.nix
+++ system/linux/oci/networks.nix
@@ -8,17 +8,27 @@
   json = pkgs.formats.json {};
 
   mkNetwork = name: network: let
-    networkConfig = {
-      inherit name;
-      inherit (network) driver id subnets internal;
-      network_interface = network.interface;
-      created = "2020-01-01T06:00:00.000000000+01:00";
-      ipam_options.driver = "host-local";
-      ipv6_enabled = network.ipv6;
-      # Using aardvark dns currently does not work, hostnames are not resolved
-      # https://github.com/containers/podman/pull/17578
-      dns_enabled = false;
-    };
+    networkConfig =
+      {
+        inherit name;
+        inherit (network) driver id subnets internal;
+        network_interface = network.interface;
+        created = "2020-01-01T06:00:00.000000000+01:00";
+        ipam_options.driver = "host-local";
+        ipv6_enabled = network.ipv6;
+        # Using aardvark dns currently does not work, hostnames are not resolved
+        # https://github.com/containers/podman/pull/17578
+        dns_enabled = false;
+      }
+      // (lib.optionalAttrs (network.driver == "bridge") {
+        options = {
+          metric = builtins.toString network.metric;
+          no_default_route =
+            if network.defaultRoute
+            then "false"
+            else "true";
+        };
+      });
   in
     lib.mkIf network.enable (
       json.generate "${name}.json" networkConfig
@@ -30,6 +40,13 @@ in {
       type = types.attrsOf (types.submodule {
         options = {
           enable = mkOption {
+            type = with types; bool;
+            default = true;
+          };
+          metric = mkOption {
+            type = with types; int;
+          };
+          defaultRoute = mkOption {
             type = with types; bool;
             default = true;
           };
