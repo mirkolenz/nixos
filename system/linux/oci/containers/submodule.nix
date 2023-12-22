edf7f7c7c74b92f3f9ef0a7d74d286b3cc13749e
Mirko Lenz <mirko@mirkolenz.com>
2023-12-22 11:26:40 +0100

    fix(oci): update default pull policy

--- system/linux/oci/containers/submodule.nix
+++ system/linux/oci/containers/submodule.nix
@@ -52,7 +52,15 @@ cfg: {lib, ...}: {
 
     pull = mkOption {
       type = with types; str;
-      default = "newer";
+      default = "missing";
+      description = lib.mdDoc ''
+        Pull image policy. The default is missing.
+
+        - always: Always pull the image and throw an error if the pull fails.
+        - missing: Pull the image only when the image is not in the local containers storage. Throw an error if no image is found and the pull fails.
+        - never: Never pull the image but use the one from the local containers storage. Throw an error if no image is found.
+        - newer: Pull if the image on the registry is newer than the one in the local containers storage. An image is considered to be newer when the digests are different. Comparing the time stamps is prone to errors. Pull errors are suppressed if a local image was found.
+      '';
     };
 
     userns = mkOption {
