9da6ed49a4f5b8fd7cf9b67dad5ab5aaa1e0c27d
Mirko Lenz <mirko@mirkolenz.com>
2024-01-08 20:09:41 +0100

    feat(lib): add some helper functions

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,19 @@
 lib: rec {
   stableVersion = "23.11";
   unstableVersion = "24.05";
-  isStable = checkedLib: lib.versionOlder checkedLib.trivial.release unstableVersion;
-  isUnstable = checkedLib: lib.versionAtLeast checkedLib.trivial.release unstableVersion;
+  isStable = lib': lib.versionOlder lib'.trivial.release unstableVersion;
+  isUnstable = lib': lib.versionAtLeast lib'.trivial.release unstableVersion;
+  channel = lib':
+    if (isStable lib')
+    then "stable"
+    else "unstable";
+  systemInput = {
+    inputs,
+    prefix,
+    channel,
+    os,
+  }:
+    inputs."${prefix}-${os}-${channel}";
   systemOs = system: lib.last (lib.splitString "-" system);
   systemArch = system: builtins.head (lib.splitString "-" system);
 }
