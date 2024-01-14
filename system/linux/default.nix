28d01e27766dda82b03e8b5e944af2d77473349c
Mirko Lenz <mirko@mirkolenz.com>
2024-01-14 23:10:50 +0100

    fix(linux): disable nix channels

--- system/linux/default.nix
+++ system/linux/default.nix
@@ -16,6 +16,7 @@
     nixPath = [
       # "nixos-config=${self.outPath}/flake.nix"
     ];
+    channel.enable = false;
     settings = {
       # https://github.com/NixOS/nix/issues/7273#issuecomment-1310213986
       auto-optimise-store = true;
