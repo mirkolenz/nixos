4d787e15068c21d9a3978d470bf194e64c785c37
Mirko Lenz <mirko@mirkolenz.com>
2024-01-17 10:47:46 +0100

    feat: improve compat overlay
    use poetry and btop from stable again

--- overlays/compat.nix
+++ overlays/compat.nix
@@ -1,1 +1,13 @@
-final: prev: {}
+final: prev: let
+  useChannel = channel: names: prev.lib.genAttrs names (name: final.${channel}.${name});
+in
+  (
+    useChannel "stable" [
+      "btop"
+      "poetry"
+    ]
+  )
+  // (
+    useChannel "unstable" [
+    ]
+  )
