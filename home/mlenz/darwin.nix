0ff53a8f3211b2f283d68fa35ea9a6e3f8c5744a
Mirko Lenz <mirko@mirkolenz.com>
2024-01-27 16:16:50 +0100

    feat(darwin): add selected binaries to ~/bin

--- home/mlenz/darwin.nix
+++ home/mlenz/darwin.nix
@@ -11,6 +11,20 @@ lib.mkIf pkgs.stdenv.isDarwin {
     vimr-bin
     restic-browser-bin
   ];
+  # add binaries for desktop apps to ~/bin
+  # currently required for the following apps:
+  # restic-browser, vim guis
+  home.file."bin".source = let
+    env = pkgs.buildEnv {
+      name = "home-bin";
+      paths = with pkgs; [
+        restic
+        # https://github.com/nix-community/nixvim/blob/main/wrappers/hm.nix
+        config.programs.nixvim.finalPackage
+      ];
+      pathsToLink = ["/bin"];
+    };
+  in "${lib.getBin env}/bin";
   custom.texlive = {
     enable = true;
     bibFolder = "${config.home.homeDirectory}/Developer/mirkolenz/bibliography";
