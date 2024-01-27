090d0e7dc5dad3b796567c1f800ad0f5c6d8bb23
Mirko Lenz <mirko@mirkolenz.com>
2024-01-27 22:05:55 +0100

    feat: customize perSystem pkgs attribute

--- flake-modules/default.nix
+++ flake-modules/default.nix
@@ -1,6 +1,19 @@
-{lib', ...}: {
+{
+  lib',
+  inputs,
+  ...
+}: {
   imports = lib'.flocken.getModules ./.;
-  perSystem = {pkgs, ...}: {
+  perSystem = {
+    pkgs,
+    system,
+    ...
+  }: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      config = import ../nixpkgs-config.nix;
+      overlays = import ../overlays inputs;
+    };
     formatter = pkgs.alejandra;
   };
 }
