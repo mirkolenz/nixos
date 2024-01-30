2513aa5bafe9a93d9824a48cf19c9c1114afae35
Mirko Lenz <mirko@mirkolenz.com>
2024-01-30 10:01:19 +0100

    fix: remove binary cache from hm standalone

--- home/mlenz/nix.nix
+++ home/mlenz/nix.nix
@@ -8,9 +8,5 @@ args@{
   nix = lib.mkIf (osConfig == { }) {
     package = pkgs.nix;
     registry = import ../../registry.nix args;
-    settings = {
-      substituters = [ "https://mirkolenz.cachix.org" ];
-      trusted-public-keys = [ "mirkolenz.cachix.org-1:R0dgCJ93t33K/gncNbKgUdJzwgsYVXeExRsZNz5jpho=" ];
-    };
   };
 }
