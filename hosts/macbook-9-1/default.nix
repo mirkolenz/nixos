431b42b5bcb8aacec848ddbe71be168f52b8d7cd
Mirko Lenz <mirko@mirkolenz.com>
2024-02-25 18:39:07 +0100

    chore: update macbook-9-1 modules

--- hosts/macbook-9-1/default.nix
+++ hosts/macbook-9-1/default.nix
@@ -2,7 +2,8 @@
 {
   imports = [
     inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
-    inputs.nixos-hardware.nixosModules.common-cpu-intel
+    inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
     ./hardware.nix
     ../../profiles/workstation.nix
   ];
