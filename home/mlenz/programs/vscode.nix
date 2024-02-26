a816cdf5504a300e4184bd689b2a84057f7ad8a1
Mirko Lenz <mirko@mirkolenz.com>
2024-02-26 21:16:59 +0100

    fix: no longer use fhs version of vscode

--- home/mlenz/programs/vscode.nix
+++ home/mlenz/programs/vscode.nix
@@ -7,6 +7,6 @@
 lib.mkIf (pkgs.stdenv.isLinux && (osConfig.services.xserver.enable or false)) {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode-fhs;
+    package = pkgs.vscode;
   };
 }
