e2ea7dee80919c4680b17c025d144538fef81af6
Mirko Lenz <mirko@mirkolenz.com>
2024-02-26 14:14:44 +0100

    feat: add 1password ssh agent config

--- home/mlenz/files/default.nix
+++ home/mlenz/files/default.nix
@@ -1,4 +1,12 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}:
+let
+  tomlFormat = pkgs.formats.toml { };
+in
 {
   home.file = {
     ".mackup.cfg" = lib.mkIf pkgs.stdenv.isDarwin { source = ./.mackup.cfg; };
@@ -8,6 +16,19 @@
     configFile = {
       "macchina/themes/custom.toml".source = ./macchina-theme.toml;
       "macchina/macchina.toml".source = ./macchina-config.toml;
+      # https://developer.1password.com/docs/ssh/agent/config
+      "1Password/ssh/agent.toml" =
+        lib.mkIf (pkgs.stdenv.isLinux && osConfig.programs._1password-gui.enable or false)
+          {
+            source = tomlFormat.generate "1password-ssh-agent" {
+              ssh-keys = [
+                {
+                  vault = "Mirko";
+                  item = "Mirkos NixBook SSH Key";
+                }
+              ];
+            };
+          };
     };
   };
 }
