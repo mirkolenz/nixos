5b4b4700f1e1bb1eea996cd925e438e437e9582c
Mirko Lenz <mirko@mirkolenz.com>
2024-02-26 11:39:24 +0100

    fix: use stable nvfetcher

--- overlays/compat.nix
+++ overlays/compat.nix
@@ -1,7 +1,7 @@
 final: prev:
 let
   useChannel = channel: names: prev.lib.genAttrs names (name: final.${channel}.${name});
-  stablePkgs = [ ];
+  stablePkgs = [ "nvfetcher" ];
   unstablePkgs = [ ];
 in
 (useChannel "stable" stablePkgs) // (useChannel "unstable" unstablePkgs)
