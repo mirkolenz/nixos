5ed86a16a4c3264f033e75a6d1786668d022f11c
Mirko Lenz <mirko@mirkolenz.com>
2024-02-26 20:38:58 +0100

    chore: disable auto login

--- system/linux/xserver.nix
+++ system/linux/xserver.nix
@@ -26,26 +26,17 @@ lib.mkIf config.services.xserver.enable {
       ]);
   };
   services.xserver = {
-    displayManager = {
-      gdm.enable = true;
-      autoLogin = {
-        enable = true;
-        user = user.login;
-      };
+    displayManager.gdm = {
+      enable = true;
     };
-    desktopManager = {
-      gnome.enable = true;
+    desktopManager.gnome = {
+      enable = true;
     };
     layout = "us";
     xkbVariant = "";
     excludePackages = with pkgs; [ xterm ];
   };
-  # Enable/disable default GNOME apps.
   services.gnome.core-utilities.enable = false;
   security.pam.services.gdm.enableGnomeKeyring = true;
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-  # Gnome themes
   programs.dconf.enable = true;
 }
