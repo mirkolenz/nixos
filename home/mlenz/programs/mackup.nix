dededaf5fffe3b91857a71ba00bd0b97bcdadf5e
Mirko Lenz <mirko@mirkolenz.com>
2024-03-01 13:16:21 +0100

    feat: improve mackup integration

--- home/mlenz/programs/mackup.nix
+++ home/mlenz/programs/mackup.nix
@@ -1,31 +1,40 @@
 { pkgs, lib, ... }:
 let
   iniFormat = pkgs.formats.ini { };
+  mkList = values: lib.genAttrs values (_: "");
+
+  builtinApps = [
+    "bartender"
+    "contexts"
+    "coteditor"
+    "defaultkeybinding"
+    "fork"
+    "iterm2"
+    "tower"
+  ];
+  customApps = {
+    # test = {
+    #   application.name = "Test";
+    #   configuration_files = mkList [ ".test" ];
+    # };
+  };
 in
 lib.mkIf pkgs.stdenv.isDarwin {
-  home.file.".mackup.cfg".source = iniFormat.generate "mackup-config" {
-    storage.engine = "icloud";
-    applications_to_sync =
-      lib.genAttrs
-        [
-          "bartender"
-          "bibdesk"
-          "default-folder-x"
-          "defaultkeybinding"
-          "docker"
-          "forklift"
-          "iina"
-          "istat-menus"
-          "iterm2"
-          "mackup"
-          "postico"
-          "rstudio"
-          "sublime-text-3"
-          "tableplus"
-          "xcode"
-        ]
-        (name: null);
-  };
-  # currently not packaged
+  home.file =
+    {
+      ".mackup.cfg".source = iniFormat.generate "mackup-config" {
+        storage.engine = "icloud";
+        applications_to_sync = mkList (builtinApps ++ builtins.attrNames customApps);
+      };
+    }
+    // (lib.mapAttrs'
+      (name: value: {
+        name = ".mackup/${name}.cfg";
+        value = {
+          source = iniFormat.generate "mackup-config-${name}" value;
+        };
+      })
+      customApps
+    );
   home.packages = with pkgs; [ mackup ];
 }
