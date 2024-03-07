2b18e65474063ac297eab48b8dbe5020a59a5ca6
Mirko Lenz <mirko@mirkolenz.com>
2024-03-07 17:07:17 +0100

    chore(macpro): add samba autodiscovery

--- hosts/macpro/samba.nix
+++ hosts/macpro/samba.nix
@@ -1,12 +1,15 @@
-{ user, ... }:
+{ user, pkgs, ... }:
 {
   # For a user to be authenticated on the samba server,
   # you must add their password using smbpasswd -a <user> as root.
+  # https://gist.github.com/vy-let/a030c1079f09ecae4135aebf1e121ea6
   services.samba = {
     enable = true;
+    package = pkgs.samba4Full;
     openFirewall = true;
     securityType = "user";
     extraConfig = ''
+      server smb encrypt = required
       server string = homeserver
       hosts allow = 10.16.0.1/16 127.0.0.1 localhost
       hosts deny = 0.0.0.0/0
@@ -27,5 +30,17 @@
         browseable = "yes";
       };
     };
+  };
+  # for windows network discovery
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+  # for mac network discovery
+  services.avahi = {
+    enable = true;
+    openFirewall = true;
+    publish.enable = true;
+    publish.userServices = true;
   };
 }
