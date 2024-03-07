0047c15f8630fab016e8980d9033bb8b79a81bea
Mirko Lenz <mirko@mirkolenz.com>
2024-03-07 16:51:29 +0100

    chore(macpro): enable samba

--- hosts/macpro/samba.nix
+++ hosts/macpro/samba.nix
@@ -1,51 +1,31 @@
-{ ... }:
+{ user, ... }:
 {
   # For a user to be authenticated on the samba server,
   # you must add their password using smbpasswd -a <user> as root.
   services.samba = {
-    # openFirewall = true;
+    enable = true;
+    openFirewall = true;
     securityType = "user";
     extraConfig = ''
-      workgroup = WORKGROUP
-      server string = homesamba
-      netbios name = homesamba
-      security = user
+      server string = homeserver
       hosts allow = 10.16.0.1/16 127.0.0.1 localhost
       hosts deny = 0.0.0.0/0
       guest account = nobody
       map to guest = bad user
+      fruit:aapl = yes
+      fruit:model = MacPro
+      fruit:advertise_fullsync = true
     '';
     shares = {
-      tm_share = {
-        path = "/mnt/Shares/tm_share";
-        "valid users" = "username";
+      timemachine = {
+        path = "/mnt/backup/timemachine";
+        "valid users" = user.login;
         public = "no";
         writeable = "yes";
-        "force user" = "username";
-        "fruit:aapl" = "yes";
         "fruit:time machine" = "yes";
         "vfs objects" = "catia fruit streams_xattr";
+        browseable = "yes";
       };
-      # public = {
-      #   path = "/mnt/Shares/Public";
-      #   browseable = "yes";
-      #   "read only" = "no";
-      #   "guest ok" = "yes";
-      #   "create mask" = "0644";
-      #   "directory mask" = "0755";
-      #   "force user" = "username";
-      #   "force group" = "groupname";
-      # };
-      # private = {
-      #   path = "/mnt/Shares/Private";
-      #   browseable = "yes";
-      #   "read only" = "no";
-      #   "guest ok" = "no";
-      #   "create mask" = "0644";
-      #   "directory mask" = "0755";
-      #   "force user" = "username";
-      #   "force group" = "groupname";
-      # };
     };
   };
 }
