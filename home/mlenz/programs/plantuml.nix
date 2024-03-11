0ff39649fe4cc3f1997ea29c07eb182024862b11
Mirko Lenz <mirko@mirkolenz.com>
2024-03-11 21:36:09 +0100

    feat: add plantuml template

---
+++ home/mlenz/programs/plantuml.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  home.file.".plantuml/init.puml".text = ''
+    skinparam Monochrome true
+    skinparam Shadowing false
+    skinparam ComponentStyle uml2
+
+    skinparam DefaultFontName Palatino
+    ' skinparam DefaultFontName Fira Mono
+    skinparam TitleFontName Fira Sans
+    skinparam TitleFontStyle Bold
+  '';
+}
