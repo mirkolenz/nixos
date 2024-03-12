47e246b5cbe26cb26bfb240bb68a9215f6215354
Mirko Lenz <mirko@mirkolenz.com>
2024-03-12 11:07:34 +0100

    refactor(macpro): rename samba to timemachine

--- hosts/macpro/timemachine.nix
+++ hosts/macpro/timemachine.nix
@@ -1,8 +1,16 @@
-{ user, pkgs, ... }:
+{ pkgs, ... }:
 {
   # For a user to be authenticated on the samba server,
   # you must add their password using smbpasswd -a <user> as root.
   # https://gist.github.com/vy-let/a030c1079f09ecae4135aebf1e121ea6
+  # users = {
+  #   users.timemachine = {
+  #     isSystemUser = true;
+  #     uid = 510;
+  #     group = "timemachine";
+  #   };
+  #   groups.timemachine.gid = 510;
+  # };
   services.samba = {
     enable = true;
     package = pkgs.samba4Full;
@@ -21,13 +29,17 @@
     '';
     shares = {
       timemachine = {
+        # path = "/mnt/backup/timemachine";
+        # "valid users" = "timemachine";
+        # "force user" = "timemachine";
+        # "force group" = "timemachine";
         path = "/mnt/backup/timemachine/%U";
         "valid users" = "%U";
         public = "no";
         writeable = "yes";
         "fruit:time machine" = "yes";
-        "vfs objects" = "catia fruit streams_xattr";
         browseable = "yes";
+        "vfs objects" = "catia fruit streams_xattr";
       };
     };
   };
