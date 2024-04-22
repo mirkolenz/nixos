6004421d866f55d70cc0b6e5efb62e20f3ef5db9
Mirko Lenz <mirko@mirkolenz.com>
2024-04-22 13:36:21 +0200

    feat(home): use upstream poetry module

--- home/mlenz/programs/poetry.nix
+++ home/mlenz/programs/poetry.nix
@@ -1,17 +1,1 @@
-{ pkgs, ... }:
-{
-  programs.poetry = {
-    enable = true;
-    package = pkgs.poetry.withPlugins (ps: with ps; [ poetry-plugin-up ]);
-    settings = {
-      virtualenvs = {
-        in-project = true;
-        prefer-active-python = true;
-        options = {
-          no-pip = true;
-          no-setuptools = true;
-        };
-      };
-    };
-  };
-}
+{ pkgs, ... }: { }
