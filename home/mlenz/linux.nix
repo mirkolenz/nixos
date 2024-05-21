0e5d86e22c4a68f903cdc04f0468d98fe8da8d27
Mirko Lenz <mirko@mirkolenz.com>
2024-05-21 09:51:05 +0200

    feat: add script getusers

--- home/mlenz/linux.nix
+++ home/mlenz/linux.nix
@@ -3,5 +3,12 @@ lib.mkIf pkgs.stdenv.isLinux {
   home.packages = with pkgs; [
     fastfetch
     cfspeedtest
+    # https://unix.stackexchange.com/a/617686
+    (writeShellApplication {
+      name = "getusers";
+      text = ''
+        ${lib.getExe' procps "ps"} -eo user,uid | ${lib.getExe gawk} 'NR>1 && $2 >= 1000 && ++seen[$2]==1{print $1}'
+      '';
+    })
   ];
 }
