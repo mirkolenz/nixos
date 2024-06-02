31943f6154bd8de44484dc664dcfddc5d4015f1b
Mirko Lenz <mirko@mirkolenz.com>
2024-06-02 18:51:48 +0200

    fix: rework nix caches module

--- system/common/options/caches.nix
+++ system/common/options/caches.nix
@@ -1,26 +1,19 @@
 { lib, config, ... }:
 let
-  cfg = config.custom.caches;
+  cfg = config.custom.nix-binary-caches;
 in
 {
-  options.custom.caches =
+  options.custom.nix-binary-caches =
     with lib;
     mkOption {
       description = "Binary caches to use for fetching packages.";
-      type = types.listOf (
-        types.submodule {
-          options = {
-            url = mkOption { type = types.str; };
-            key = mkOption { type = types.str; };
-          };
-        }
-      );
+      type = with types; attrsOf str;
       default = { };
     };
   config = lib.mkIf (cfg != { }) {
     nix.settings = {
-      substituters = map (c: c.url) cfg;
-      trusted-public-keys = map (c: c.key) cfg;
+      substituters = builtins.attrNames cfg;
+      trusted-public-keys = builtins.attrValues cfg;
     };
   };
 }
