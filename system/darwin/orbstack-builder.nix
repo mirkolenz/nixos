e5000ef7309f609dcd5148c7af6c8b339ea53331
Mirko Lenz <mirko@mirkolenz.com>
2024-06-03 13:45:01 +0200

    feat(darwin): add orbstack builder

---
+++ system/darwin/orbstack-builder.nix
@@ -0,0 +1,25 @@
+{ user, stdenv, ... }:
+{
+  # https://github.com/LnL7/nix-darwin/blob/master/modules/nix/linux-builder.nix
+  # https://docs.orbstack.dev/machines/ssh
+  nix = {
+    distributedBuilds = true;
+    settings.builders-use-substitutes = true;
+    buildMachines = [
+      {
+        hostName = "nixos@orb";
+        sshUser = "mlenz";
+        # sshKey = "/Users/${user.login}/.orbstack/ssh/id_ed25519";
+        # publicHostKey = null;
+        maxJobs = 4;
+        protocol = "ssh-ng";
+        supportedFeatures = [
+          "kvm"
+          "benchmark"
+          "big-parallel"
+        ];
+        systems = [ "${stdenv.hostPlatform.uname.processor}-linux" ];
+      }
+    ];
+  };
+}
