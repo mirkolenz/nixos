6db27c9a063f72d73e78bc5cf2fb404f2cf4aa37
Mirko Lenz <mirko@mirkolenz.com>
2024-06-05 14:49:56 +0200

    fix(darwin): update orbstack builder config

--- system/darwin/orbstack-builder.nix
+++ system/darwin/orbstack-builder.nix
@@ -2,17 +2,23 @@
 {
   # https://github.com/LnL7/nix-darwin/blob/master/modules/nix/linux-builder.nix
   # https://docs.orbstack.dev/machines/ssh
+  environment.etc."ssh/ssh_config.d/100-orbstack-builder.conf".text = ''
+    Host orbstack-builder
+      Hostname 127.0.0.1
+      Port 32222
+  '';
   nix = {
     distributedBuilds = true;
     settings.builders-use-substitutes = true;
     buildMachines = [
       {
-        hostName = "nixos@orb";
-        sshUser = "mlenz";
-        # sshKey = "/Users/${user.login}/.orbstack/ssh/id_ed25519";
-        # publicHostKey = null;
+        hostName = "orbstack-builder";
+        sshUser = "nixos";
+        sshKey = "/Users/${user.login}/.orbstack/ssh/id_ed25519";
+        # sudo ssh nixos@orbstack-builder
+        publicHostKey = null;
         maxJobs = 4;
-        protocol = "ssh-ng";
+        protocol = "ssh";
         supportedFeatures = [
           "kvm"
           "benchmark"
