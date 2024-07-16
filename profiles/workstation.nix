7cb60ab2b40a20b00aa164e4c4b3d3bce29bd387
Mirko Lenz <mirko@mirkolenz.com>
2024-07-16 09:33:38 +0200

    fix: remove deprecated sound option

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -4,10 +4,6 @@
   services.printing.enable = true;
 
   security.rtkit.enable = config.services.pipewire.enable;
-  sound = {
-    enable = true;
-    mediaKeys.enable = true;
-  };
   hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
