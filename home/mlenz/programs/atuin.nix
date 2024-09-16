b15dfc5e75cd8934d98ac721e813e72c712553de
Mirko Lenz <mirko@mirkolenz.com>
2024-09-16 14:37:47 +0200

    fix: re-enable atuin

--- home/mlenz/programs/atuin.nix
+++ home/mlenz/programs/atuin.nix
@@ -1,13 +1,17 @@
 { ... }:
 {
   programs.atuin = {
-    enable = false;
+    enable = true;
+    # https://docs.atuin.sh/configuration/config/
     settings = {
       auto_sync = true;
+      sync_frequency = "1h";
       update_check = false;
       search_mode = "fuzzy";
       enter_accept = true;
       keymap_mode = "vim-insert";
+      sync.records = true;
+      dotfiles.enabled = false;
     };
   };
 }
