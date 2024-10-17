01550258d137ab37442afd165959361bbc8d3830
Mirko Lenz <mirko@mirkolenz.com>
2024-10-17 09:40:23 +0200

    fix(cuda): remove deprecated binary cache

--- system/linux/options/cuda.nix
+++ system/linux/options/cuda.nix
@@ -35,11 +35,5 @@ in
         modesetting.enable = true;
       };
     };
-    nix.settings = {
-      substituters = [ "https://cuda-maintainers.cachix.org" ];
-      trusted-public-keys = [
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      ];
-    };
   };
 }
