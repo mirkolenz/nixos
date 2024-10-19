e362b367640b761b5de1c763b6c9a9fbfa89c9b3
Mirko Lenz <mirko@mirkolenz.com>
2024-10-19 16:42:10 +0200

    chore(ssh): update hostNames

--- system/common/ssh.nix
+++ system/common/ssh.nix
@@ -9,6 +9,7 @@
     macpro = {
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP5n2KpAU3E8iHy56vNURjh7E3l0EjkZ6BX450Z44hiH";
       hostNames = [
+        "macpro"
         "10.16.2.22"
         "100.87.219.52"
       ];
@@ -16,6 +17,7 @@
     raspi = {
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGR8BCsN02425GzSv1PQNhUGx0rm8D4aKSZh8ut5OJ6/";
       hostNames = [
+        "raspi"
         "10.16.2.23"
         "100.71.1.54"
       ];
