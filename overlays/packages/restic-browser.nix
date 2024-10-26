5c2628c866fcdb9abf94e15fed02b777f27e4e82
Mirko Lenz <mirko@mirkolenz.com>
2024-10-26 21:03:40 +0200

    fix(restic-browser): update fetcher

--- overlays/packages/restic-browser.nix
+++ overlays/packages/restic-browser.nix
@@ -9,13 +9,13 @@ mkApp rec {
   appname = "Restic-Browser";
   src = fetchzip {
     url = "https://github.com/emuell/restic-browser/releases/download/v${version}/${appname}-v${version}-macOS.zip";
-    hash = "sha256-KYxf+dtpmenJLc7/9QeB8Q5d6+jdqVqjRIGfLc+KUiY=";
+    hash = "sha256-7fyJUuKLS9ed6zT1kefJQNxICudFbvrVRx5Pgxw6QIY=";
+    # the downloaded zip contains a tar archive instead of the app
+    postFetch = ''
+      tar -xf "$out/${appname}.app.tar" -C "$out"
+      rm "$out/${appname}.app.tar"
+    '';
   };
-  # this is needed because the downloaded zip contains a tar archive instead of the app
-  unpackCmd = ''
-    tar -xf "$curSrc/${appname}.app.tar" -C .
-  '';
-  sourceRoot = ".";
   meta = {
     description = "A GUI to browse and restore restic backup repositories.";
     homepage = "https://github.com/emuell/restic-browser";
