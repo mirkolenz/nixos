7b84da3cc4f155851ea9ad8444ef40671e43d3e8
Mirko Lenz <mirko@mirkolenz.com>
2024-11-03 22:04:32 +0100

    fix(darwin): remove deprecated loginShell

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -7,8 +7,6 @@
 {
   imports = [ ../common ] ++ (lib'.flocken.getModules ./.);
 
-  environment.loginShell = pkgs.fish;
-
   environment.systemPackages = with pkgs; [ _1password ];
 
   system.stateVersion = stateVersionDarwin;
