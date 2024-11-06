6ffb72639d3abd53140b031d24d647edb269ad5b
Mirko Lenz <mirko@mirkolenz.com>
2024-11-06 16:41:20 +0100

    refactor(home/darwin): improve legibility of link farms

--- home/mlenz/darwin.nix
+++ home/mlenz/darwin.nix
@@ -23,31 +23,33 @@ lib.mkIf pkgs.stdenv.isDarwin {
   # currently required for the following apps:
   # restic-browser, git guis, vim guis
   home.file = {
-    "bin".source = pkgs.linkFarm "home-bin" (
-      lib.mapAttrsToList
-        (name: value: {
-          inherit name;
-          path = lib.getExe value;
-        })
-        {
-          restic = pkgs.restic;
-          git = config.programs.git.package;
-          nvim = config.custom.neovim.package;
-        }
-    );
+    "bin".source =
+      let
+        mkPaths = lib.mapAttrsToList (
+          name: value: {
+            inherit name;
+            path = lib.getExe value;
+          }
+        );
+      in
+      pkgs.linkFarm "home-bin" (mkPaths {
+        restic = pkgs.restic;
+        git = config.programs.git.package;
+        nvim = config.custom.neovim.package;
+      });
     # add libraries for desktop apps to ~/node_modules
     # currently required for the following apps:
     # vscode/prettier
-    "node_modules".source = pkgs.linkFarm "home-node-modules" (
-      map
-        (name: {
+    "node_modules".source =
+      let
+        mkPaths = map (name: {
           inherit name;
           path = "${lib.getLib pkgs.nodePackages.${name}}/lib/node_modules/${name}";
-        })
-        [
-          "prettier"
-        ]
-    );
+        });
+      in
+      pkgs.linkFarm "home-node-modules" (mkPaths [
+        "prettier"
+      ]);
     # add entries to the local dictionary
     "Library/Group Containers/group.com.apple.AppleSpell/Library/Spelling/LocalDictionary".text = ''
       mirkolenz
