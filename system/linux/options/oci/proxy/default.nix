913d99c62a20bc90c238c9349524da7824aab4cc
Mirko Lenz <mirko@mirkolenz.com>
2024-11-11 08:49:47 +0100

    fix: update caddyfile formatting

--- system/linux/options/oci/proxy/default.nix
+++ system/linux/options/oci/proxy/default.nix
@@ -78,7 +78,7 @@ let
   '';
 
   Caddyfile-formatted =
-    pkgs.runCommand "Caddyfile-formatted" { nativeBuildInputs = [ pkgs.custom-caddy ]; }
+    pkgs.runCommand "Caddyfile-formatted" { nativeBuildInputs = [ pkgs.caddy ]; }
       ''
         mkdir -p $out
         cp --no-preserve=mode ${Caddyfile-raw}/Caddyfile $out/Caddyfile
