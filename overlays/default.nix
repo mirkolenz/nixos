007b1c7db95d22967ba8d7946d2396cbdbc82c34
Mirko Lenz <mirko@mirkolenz.com>
2024-11-17 19:49:52 +0100

    chore: remove poetry2nix overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@ args@{ inputs, ... }:
 [
   (import ./channels args)
   (import ./packages args)
-  inputs.poetry2nix.overlays.default
   (final: prev: inputs.caddy-nix.overlays.default final.nixpkgs prev.nixpkgs)
   inputs.nix-darwin-unstable.overlays.default
 ]
