ee68910f93ce7a4f14c74bd09771ea94cc199269
Mirko Lenz <mirko@mirkolenz.com>
2024-11-20 08:28:01 +0100

    refactor: update channel overlay

--- overlays/channels/default.nix
+++ overlays/channels/default.nix
@@ -7,33 +7,34 @@
 final: prev:
 let
   inherit (final.pkgs) system;
+  inherit (prev) lib;
   os = lib'.self.systemOs system;
-
-  mkChannel =
-    channel:
-    let
-      nixpkgs = lib'.self.systemInput {
-        inherit inputs channel os;
-        name = "nixpkgs";
-      };
-    in
-    import nixpkgs {
-      inherit system;
-      config = nixpkgsConfig;
-    };
-  useChannel = channel: names: prev.lib.genAttrs names (name: final.${channel}.${name});
-
-  packages = import ./packages.nix;
 in
-(prev.lib.genAttrs [
-  "stable"
-  "unstable"
-] mkChannel)
-// (useChannel "stable" packages.stable)
-// (useChannel "unstable" packages.unstable)
-// {
+{
   nixpkgs = import inputs.nixpkgs {
     inherit system;
     config = nixpkgsConfig;
   };
 }
+// (lib.genAttrs
+  [
+    "stable"
+    "unstable"
+  ]
+  (
+    channel:
+    let
+      nixpkgsInput = lib'.self.systemInput {
+        inherit inputs channel os;
+        name = "nixpkgs";
+      };
+    in
+    import nixpkgsInput {
+      inherit system;
+      config = nixpkgsConfig;
+    }
+  )
+)
+// (lib.mapAttrs (
+  channel: packages: lib.genAttrs packages (package: final.${channel}.${package})
+) (import ./overrides.nix))
