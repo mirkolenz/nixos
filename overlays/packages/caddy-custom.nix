2a5d6488887741c4565ab8219dcd507a287026c1
Mirko Lenz <mirko@mirkolenz.com>
2024-12-28 11:13:21 +0100

    chore: update caddy hash

--- overlays/packages/caddy-custom.nix
+++ overlays/packages/caddy-custom.nix
@@ -3,5 +3,5 @@ caddy.withPlugins {
   plugins = [
     "github.com/caddy-dns/cloudflare@89f16b99c18ef49c8bb470a82f895bce01cbaece"
   ];
-  hash = "sha256-Aqu2st8blQr/Ekia2KrH1AP/2BVZIN4jOJpdLc1Rr4g=";
+  hash = "sha256-WGV/Ve7hbVry5ugSmTYWDihoC9i+D3Ct15UKgdpYc9U=";
 }
