bfbc96d4aeb49278fb91964613eecf02a3e049bc
Mirko Lenz <mirko@mirkolenz.com>
2025-01-03 16:34:37 +0100

    chore: fix hash for restic-browser

--- overlays/packages/restic-browser.nix
+++ overlays/packages/restic-browser.nix
@@ -9,7 +9,7 @@ mkApp rec {
   appname = "Restic-Browser";
   src = fetchzip {
     url = "https://github.com/emuell/restic-browser/releases/download/v${version}/${appname}-v${version}-macOS.zip";
-    hash = "sha256-2xQzN3Twnuj2vLW5JckJrG4JTirz9q1p5D0l6Hgrrec=";
+    hash = "sha256-3uPmdJZmUPfQjVCuf5h9Wa/rzezJqgfqsDz0vdcf5AM=";
     # the downloaded zip contains a tar archive instead of the app
     postFetch = ''
       tar -xf "$out/${appname}.app.tar" -C "$out"
