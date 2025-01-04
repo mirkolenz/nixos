dc978335694fbe3435641e13c4f2b3020b1ed501
Mirko Lenz <mirko@mirkolenz.com>
2025-01-04 10:49:46 +0100

    chore: update ghostty

--- home/mlenz/programs/ghostty.nix
+++ home/mlenz/programs/ghostty.nix
@@ -4,12 +4,12 @@
     enable = pkgs.stdenv.isDarwin;
     settings = {
       cursor-click-to-move = true;
-      font-family = "TX-02";
+      font-family = "Berkeley Mono";
       font-size = 13;
       font-thicken = true;
       shell-integration = "none";
       shell-integration-features = "no-cursor,sudo,title";
-      theme = "Builtin Dark";
+      theme = "dark:GitHub Dark,light:GitHub";
       window-height = 30;
       window-padding-x = 8;
       window-padding-y = 8;
