d3a0080b61be9fcba24408e6fd8bbc03736c1b23
Mirko Lenz <mirko@mirkolenz.com>
2025-01-07 14:30:59 +0100

    fix(ghostty): improve settings

--- home/mlenz/programs/ghostty.nix
+++ home/mlenz/programs/ghostty.nix
@@ -1,15 +1,38 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  mkSettings = lib.mapAttrs (
+    _: value:
+    if lib.isList value then
+      lib.concatStringsSep "," value
+    else if lib.isAttrs value then
+      lib.concatMapAttrsStringSep "," (k: v: "${k}:${v}") value
+    else
+      value
+  );
+in
 {
   programs.ghostty-custom = {
     enable = pkgs.stdenv.isDarwin;
-    settings = {
+    settings = mkSettings {
       cursor-click-to-move = true;
       font-family = "Berkeley Mono";
       font-size = 13;
       font-thicken = true;
       shell-integration = "none";
-      shell-integration-features = "no-cursor,sudo,title";
-      theme = "dark:GitHub Dark,light:GitHub";
+      shell-integration-features = [
+        "no-cursor"
+        "sudo"
+        "title"
+      ];
+      theme = {
+        dark = "GitHub Dark";
+        light = "GitHub";
+      };
       window-height = 30;
       window-padding-x = 8;
       window-padding-y = 8;
@@ -18,5 +41,9 @@
     enableBashIntegration = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
+  };
+
+  programs.ssh.matchBlocks."*".extraOptions = lib.mkIf config.programs.ghostty-custom.enable {
+    SetEnv = "TERM=xterm-256color";
   };
 }
