3f3058233bbd082c268f9da7d7f1efe00f797bb4
Mirko Lenz <mirko@mirkolenz.com>
2025-01-21 09:19:29 +0100

    ci: replace garnix with github actions

---
+++ flake-modules/github-actions.nix
@@ -0,0 +1,20 @@
+{
+  inputs,
+  lib,
+  self,
+  ...
+}:
+{
+  flake = {
+    githubActionsBuild = inputs.nix-github-actions.lib.mkGithubMatrix {
+      inherit (self) checks;
+    };
+    githubActionsEval = inputs.nix-github-actions.lib.mkGithubMatrix {
+      checks.x86_64-linux =
+        { }
+        // (lib.mapAttrs (name: value: value.config.system.toplevel.build) self.nixosConfigurations)
+        // (lib.mapAttrs (name: value: value.config.system.toplevel.build) self.darwinConfigurations)
+        // (lib.mapAttrs (name: value: value.config.activationPackage) self.homeConfigurations);
+    };
+  };
+}
