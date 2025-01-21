44193a1909032dc6f54e15fae00444d25138c696
Mirko Lenz <mirko@mirkolenz.com>
2025-01-21 09:46:10 +0100

    ci: update matrix generation

--- flake-modules/github-actions.nix
+++ flake-modules/github-actions.nix
@@ -4,17 +4,28 @@
   self,
   ...
 }:
+let
+  buildChecks = self.checks;
+  evalChecks = {
+    x86_64-linux =
+      { }
+      // (lib.mapAttrs (_: module: module.config.system.toplevel.build) self.nixosConfigurations)
+      // (lib.mapAttrs (_: module: module.config.system.toplevel.build) self.darwinConfigurations)
+      // (lib.mapAttrs (_: module: module.activationPackage) self.homeConfigurations);
+  };
+  allChecks = lib.recursiveUpdate evalChecks buildChecks;
+  filterChecks = checks: lib.filterAttrs [ "x86_64-linux" "x86_64-darwin" ] checks;
+in
 {
   flake = {
+    githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+      checks = filterChecks allChecks;
+    };
     githubActionsBuild = inputs.nix-github-actions.lib.mkGithubMatrix {
-      inherit (self) checks;
+      checks = filterChecks buildChecks;
     };
     githubActionsEval = inputs.nix-github-actions.lib.mkGithubMatrix {
-      checks.x86_64-linux =
-        { }
-        // (lib.mapAttrs (_: module: module.config.system.toplevel.build) self.nixosConfigurations)
-        // (lib.mapAttrs (_: module: module.config.system.toplevel.build) self.darwinConfigurations)
-        // (lib.mapAttrs (_: module: module.activationPackage) self.homeConfigurations);
+      checks = filterChecks evalChecks;
     };
   };
 }
