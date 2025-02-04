9aeccf205e0a82bf99641f94436c0715eddb6f86
Mirko Lenz <mirko@mirkolenz.com>
2025-02-04 08:17:30 +0100

    fix(overlays): properly add linux-only packages

--- overlays/packages/default.nix
+++ overlays/packages/default.nix
@@ -1,24 +1,32 @@
-{ inputs, self, ... }:
+{
+  inputs,
+  self,
+  ...
+}:
 final: prev:
 let
-  inherit (final.stdenv.hostPlatform) system;
-  flake-exports = {
-    inherit (inputs.cosmic-manager.packages.${system}) cosmic-manager;
-    bibtex2cff = final.callPackage ./bibtex2cff.nix { };
-    bibtexbrowser = final.callPackage ./bibtexbrowser.nix { };
-    bibtexbrowser2cff = final.callPackage ./bibtexbrowser2cff.nix { };
-    caddy-custom = final.callPackage ./caddy-custom.nix { };
-    goneovim-bin = final.callPackage ./goneovim.nix { };
-    hkknx-bin = final.callPackage ./hkknx.nix { };
-    hkknx-docker = final.callPackage ./hkknx-docker.nix { };
-    neohtop-bin = final.callPackage ./neohtop.nix { };
-    neovide-bin = final.callPackage ./neovide.nix { };
-    pam-watchid = final.callPackage ./pam-watchid.nix { };
-    restic-browser-bin = final.callPackage ./restic-browser.nix { };
-    uv-migrator = final.callPackage ./uv-migrator.nix { };
-    vimr-bin = final.callPackage ./vimr.nix { };
-    zigstar-multitool = final.callPackage ./zigstar-multitool.nix { };
-  };
+  inherit (prev.stdenv.hostPlatform) system;
+  inherit (prev) lib;
+  flake-exports =
+    {
+      bibtex2cff = final.callPackage ./bibtex2cff.nix { };
+      bibtexbrowser = final.callPackage ./bibtexbrowser.nix { };
+      bibtexbrowser2cff = final.callPackage ./bibtexbrowser2cff.nix { };
+      caddy-custom = final.callPackage ./caddy-custom.nix { };
+      goneovim-bin = final.callPackage ./goneovim.nix { };
+      hkknx-bin = final.callPackage ./hkknx.nix { };
+      hkknx-docker = final.callPackage ./hkknx-docker.nix { };
+      neohtop-bin = final.callPackage ./neohtop.nix { };
+      neovide-bin = final.callPackage ./neovide.nix { };
+      pam-watchid = final.callPackage ./pam-watchid.nix { };
+      restic-browser-bin = final.callPackage ./restic-browser.nix { };
+      uv-migrator = final.callPackage ./uv-migrator.nix { };
+      vimr-bin = final.callPackage ./vimr.nix { };
+      zigstar-multitool = final.callPackage ./zigstar-multitool.nix { };
+    }
+    // (lib.optionalAttrs (inputs.cosmic-manager.packages ? ${system}) {
+      inherit (inputs.cosmic-manager.packages.${system}) cosmic-manager;
+    });
 in
 {
   inherit flake-exports;
