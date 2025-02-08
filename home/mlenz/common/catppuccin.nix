5264a01edb214c03aff2c54be06246dd90acacbc
Mirko Lenz <mirko@mirkolenz.com>
2025-02-08 22:21:58 +0100

    fix(catpuccin): disable ifd modules

--- home/mlenz/common/catppuccin.nix
+++ home/mlenz/common/catppuccin.nix
@@ -3,5 +3,8 @@
   catppuccin = {
     flavor = "mocha";
     enable = true;
+    # the following use import-from-derivation (IDF) which is disabled
+    bottom.enable = false;
+    starship.enable = false;
   };
 }
