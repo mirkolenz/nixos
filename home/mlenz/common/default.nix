199a6479b09e1b1594a3e237b9bab5d1f36afb54
Mirko Lenz <mirko@mirkolenz.com>
2025-02-11 15:27:01 +0100

    chore: unset homebrew env var

--- home/mlenz/common/default.nix
+++ home/mlenz/common/default.nix
@@ -14,7 +14,6 @@
     homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${user.login}" else "/home/${user.login}";
     sessionVariables = {
       DIRENV_LOG_FORMAT = "";
-      HOMEBREW_AUTOREMOVE = "1";
       NIXPKGS_ALLOW_UNFREE = "1";
     };
   };
