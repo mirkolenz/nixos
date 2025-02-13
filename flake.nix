79de58c84fcc2847dd33d906126c4a255d5385c1
Mirko Lenz <mirko@mirkolenz.com>
2025-02-13 09:17:11 +0100

    refactor: update flake follows directives

--- flake.nix
+++ flake.nix
@@ -84,22 +84,28 @@
     # plist-manager
     plist-manager-unstable = {
       url = "github:z0al/plist-manager";
-      inputs.nixpkgs.follows = "nixpkgs-darwin-unstable";
-      inputs.nix-darwin.follows = "nix-darwin-unstable";
-      inputs.flake-parts.follows = "flake-parts";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-darwin-unstable";
+        nix-darwin.follows = "nix-darwin-unstable";
+        flake-parts.follows = "flake-parts";
+      };
     };
     plist-manager-stable = {
       url = "github:z0al/plist-manager";
-      inputs.nixpkgs.follows = "nixpkgs-darwin-stable";
-      inputs.nix-darwin.follows = "nix-darwin-stable";
-      inputs.flake-parts.follows = "flake-parts";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-darwin-stable";
+        nix-darwin.follows = "nix-darwin-stable";
+        flake-parts.follows = "flake-parts";
+      };
     };
 
     # cosmic
     nixos-cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
-      inputs.nixpkgs.follows = "nixpkgs-linux-unstable";
-      inputs.nixpkgs-stable.follows = "nixpkgs-linux-stable";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-linux-unstable";
+        nixpkgs-stable.follows = "nixpkgs-linux-stable";
+      };
     };
     cosmic-manager = {
       url = "github:heitoraugustoln/cosmic-manager";
@@ -113,43 +119,55 @@
     # My own
     flocken = {
       url = "github:mirkolenz/flocken/v2";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.systems.follows = "systems";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        systems.follows = "systems";
+      };
     };
     quadlet-nix = {
       url = "github:mirkolenz/quadlet-nix/v1";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-parts.follows = "flake-parts";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+      };
     };
     arguebuf = {
       url = "github:recap-utr/arguebuf-python/v2";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.flocken.follows = "flocken";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.pyproject-build-systems.follows = "pyproject-build-systems";
-      inputs.treefmt-nix.follows = "treefmt-nix";
-      inputs.systems.follows = "systems";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        flocken.follows = "flocken";
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        pyproject-build-systems.follows = "pyproject-build-systems";
+        treefmt-nix.follows = "treefmt-nix";
+        systems.follows = "systems";
+      };
     };
 
-    # Utils
+    # Python/uv
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     uv2nix = {
       url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        nixpkgs.follows = "nixpkgs";
+      };
     };
     pyproject-build-systems = {
       url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        nixpkgs.follows = "nixpkgs";
+      };
     };
+
+    # Utils
     vscode-server = {
       url = "github:nix-community/nixos-vscode-server";
       inputs.nixpkgs.follows = "nixpkgs";
