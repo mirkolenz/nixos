cb9a94c42f96a98c6214dfca9ecf7b0991b163f4
Mirko Lenz <mirko@mirkolenz.com>
2025-02-13 10:48:33 +0100

    feat(vim): use github colorscheme

--- vim/colorschemes.nix
+++ vim/colorschemes.nix
@@ -1,8 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin rec {
+      pname = "github-theme";
+      version = "1.1.2";
+      src = pkgs.fetchFromGitHub {
+        owner = "projekt0n";
+        repo = "github-nvim-theme";
+        tag = "v${version}";
+        hash = "sha256-ur/65NtB8fY0acTUN/Xw9fT813UiL3YcP4+IwkaUzTE=";
+      };
+    })
+  ];
+  extraConfigLua = ''
+    require('github-theme')
+    vim.cmd('colorscheme github_dark_default')
+  '';
   colorschemes = {
     monokai-pro = {
-      enable = true;
+      enable = false;
       settings = {
         filter = "spectrum";
       };
