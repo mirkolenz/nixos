49777a6789aafb51932fe9d6f94dadc3e723f222
Mirko Lenz <mirko@mirkolenz.com>
2025-02-16 11:20:10 +0100

    feat: add neovide settings

--- home/mlenz/common/programs/neovim.nix
+++ home/mlenz/common/programs/neovim.nix
@@ -1,10 +1,18 @@
 {
   pkgs,
+  config,
   ...
 }:
 {
   custom.neovim = {
     enable = true;
     package = pkgs.nixvim-unstable;
+  };
+  programs.neovide = {
+    enable = config.custom.profile.isDesktop;
+    settings = {
+      fork = true;
+      neovim-bin = if pkgs.stdenv.isDarwin then "${config.home.homeDirectory}/bin" else lib.getExe config.custom.neovim.package;
+    };
   };
 }
