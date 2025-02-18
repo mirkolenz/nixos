7bc486299bfbc0e8f484ab065c925c5b0e66c111
Mirko Lenz <mirko@mirkolenz.com>
2025-02-18 13:48:54 +0100

    chore(vim): add keymaps for trouble

--- vim/keymaps.nix
+++ vim/keymaps.nix
@@ -38,6 +38,42 @@
       action = "<cmd>lua vim.lsp.buf.format()<CR>";
       options.desc = "Format buffer";
     }
+    {
+      key = "<leader>xx";
+      mode = "n";
+      action = "<cmd>Trouble diagnostics toggle<CR>";
+      options.desc = "Diagnostics (Trouble)";
+    }
+    {
+      key = "<leader>xX";
+      mode = "n";
+      action = "<cmd>Trouble diagnostics toggle filter.buf=0<CR>";
+      options.desc = "Buffer Diagnostics (Trouble)";
+    }
+    {
+      key = "<leader>cs";
+      mode = "n";
+      action = "<cmd>Trouble symbols toggle focus=false<CR>";
+      options.desc = "Symbols (Trouble)";
+    }
+    {
+      key = "<leader>cl";
+      mode = "n";
+      action = "<cmd>Trouble lsp toggle focus=false win.position=right<CR>";
+      options.desc = "LSP definitions/references/... (Trouble)";
+    }
+    {
+      key = "<leader>xL";
+      mode = "n";
+      action = "<cmd>Trouble loclist toggle<CR>";
+      options.desc = "Location List (Trouble)";
+    }
+    {
+      key = "<leader>xQ";
+      mode = "n";
+      action = "<cmd>Trouble qflist toggle<CR>";
+      options.desc = "Quickfix List (Trouble)";
+    }
   ];
   # "<cmd>Telescope ${value}<CR>";
   plugins.telescope.keymaps = {
