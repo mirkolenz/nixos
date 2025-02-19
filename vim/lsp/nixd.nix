f5d50f2200376ffa648c92d0d60bbba495a109d4
Mirko Lenz <mirko@mirkolenz.com>
2025-02-19 08:22:26 +0100

    chore(vim/lsp): update nixd

--- vim/lsp/nixd.nix
+++ vim/lsp/nixd.nix
@@ -4,6 +4,7 @@
     enable = true;
     settings = {
       formatting.command = [ "nixfmt" ];
+      nixpkgs.expr = ''import (builtins.getFlake "github:mirkolenz/nixos").inputs.nixpkgs { }'';
     };
   };
 }
