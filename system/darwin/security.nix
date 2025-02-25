7e7ef7e051ef6ad5b7d79f67cad34cc969f8ceb3
Mirko Lenz <mirko@mirkolenz.com>
2025-02-25 20:50:13 +0100

    chore(darwin): update pam modules

--- system/darwin/security.nix
+++ system/darwin/security.nix
@@ -3,7 +3,10 @@
   security = {
     pam.services.sudo_local = {
       enable = true;
+      # touchIdAuth = true;
+      # watchIdAuth = true;
       text = ''
+        auth       sufficient     pam_tid.so
         auth       sufficient     ${pkgs.pam-watchid}/lib/pam_watchid.so
       '';
     };
