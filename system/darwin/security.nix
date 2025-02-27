289cc3f7405e355db6576f7da1cb0378a7ce1dee
Mirko Lenz <mirko@mirkolenz.com>
2025-02-27 08:29:20 +0100

    chore(darwin): update pam settings

--- system/darwin/security.nix
+++ system/darwin/security.nix
@@ -1,14 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 {
   security = {
     pam.services.sudo_local = {
       enable = true;
-      # touchIdAuth = true;
-      # watchIdAuth = true;
-      text = ''
-        auth       sufficient     pam_tid.so
-        auth       sufficient     ${pkgs.pam-watchid}/lib/pam_watchid.so
-      '';
+      touchIdAuth = true;
+      watchIdAuth = true;
     };
     sudo.extraConfig = ''
       Defaults env_keep -= "HOME"
