045f6e0a555707168a47e9fae4692834fe06ecb4
Mirko Lenz <mirko@mirkolenz.com>
2025-03-01 09:54:26 +0100

    fix(vim): use custom autocmd to change the working directory

--- vim/autocmd.nix
+++ vim/autocmd.nix
@@ -2,6 +2,7 @@
 {
   autoGroups = {
     autoformat.clear = true;
+    autodir.clear = true;
   };
   autoCmd = [
     {
@@ -18,6 +19,38 @@
       pattern = "*";
       group = "autoformat";
       desc = "Format buffer using LSP";
+    }
+    {
+      callback = lib.nixvim.mkRaw ''
+        function()
+          local args = vim.fn.argv()
+
+          if #args > 0 then
+            local arg = args[1]
+
+            if vim.fn.isdirectory(arg) == 1 then
+              -- Argument is a directory, change to it directly
+              vim.api.nvim_set_current_dir(arg)
+              vim.notify("Changed directory to: " .. arg, vim.log.levels.INFO)
+            elseif vim.fn.filereadable(arg) == 1 then
+              -- Argument is a file, change to its parent directory
+              local dir = vim.fn.fnamemodify(arg, ":p:h")
+              vim.api.nvim_set_current_dir(dir)
+              vim.notify("Changed directory to: " .. dir, vim.log.levels.INFO)
+            else
+              -- Argument exists but is neither a readable file nor a directory
+              vim.notify("Argument is not a valid file or directory: " .. arg, vim.log.levels.WARN)
+            end
+          else
+            -- No arguments provided, using current directory
+            vim.notify("No arguments provided, using current directory", vim.log.levels.INFO)
+          end
+        end
+      '';
+      event = "VimEnter";
+      pattern = "*";
+      group = "autodir";
+      desc = "Change to directory based on first argument";
     }
   ];
 }
