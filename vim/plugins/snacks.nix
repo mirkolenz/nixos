eb11f9a898bddc4d291c185422033ad2311b90b0
Mirko Lenz <mirko@mirkolenz.com>
2025-03-15 22:32:32 +0100

    chore(vim): update snacks config

--- vim/plugins/snacks.nix
+++ vim/plugins/snacks.nix
@@ -11,15 +11,61 @@
       };
       dashboard = {
         enabled = true;
+        preset.keys = [
+          {
+            icon = " ";
+            key = "f";
+            desc = "Find File";
+            action = ":lua Snacks.dashboard.pick('files')";
+          }
+          {
+            icon = " ";
+            key = "n";
+            desc = "New File";
+            action = ":ene | startinsert";
+          }
+          {
+            icon = " ";
+            key = "g";
+            desc = "Find Text";
+            action = ":lua Snacks.dashboard.pick('live_grep')";
+          }
+          {
+            icon = " ";
+            key = "r";
+            desc = "Recent Files";
+            action = ":lua Snacks.dashboard.pick('oldfiles')";
+          }
+          {
+            icon = " ";
+            key = "p";
+            desc = "Recent Projects";
+            action = ":lua Snacks.dashboard.pick('projects')";
+          }
+          {
+            icon = " ";
+            key = "q";
+            desc = "Quit";
+            action = ":qa";
+          }
+        ];
         sections = [
           { section = "header"; }
           {
+            # icon = " ";
+            # title = "Keymaps";
             section = "keys";
-            gap = 1;
             padding = 1;
+            # indent = 2;
           }
-          # TODO: lazy.stats not found
-          # { section = "startup"; }
+          {
+            # icon = " ";
+            # title = "Projects";
+            section = "projects";
+            padding = 1;
+            # indent = 2;
+            limit = 9;
+          }
         ];
       };
       debug = {
@@ -157,7 +203,7 @@
         }
         {
           key = "<leader>fp";
-          action = "picker.projects()";
+          action = ''picker.projects({ dev = { "~/dev", "~/Developer" }, patterns = { ".git", "flake.nix" } })'';
           options.desc = "Projects";
         }
         {
