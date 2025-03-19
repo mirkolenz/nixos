4a17c14c70d47a295defa7cdec55b489212a444e
Mirko Lenz <mirko@mirkolenz.com>
2025-03-19 08:37:00 +0100

    feat(home/macchina): convert to module and import theme from package

--- home/mlenz/common/programs/macchina.nix
+++ home/mlenz/common/programs/macchina.nix
@@ -1,54 +1,18 @@
-{ pkgs, ... }:
-let
-  tomlFormat = pkgs.formats.toml { };
-in
+{ config, ... }:
 {
-  home.packages = with pkgs; [ macchina ];
-  xdg.configFile = {
-    # https://github.com/Macchina-CLI/macchina/blob/main/contrib/themes/Lithium.toml
-    "macchina/themes/custom.toml".source = tomlFormat.generate "macchina-theme" {
-      spacing = 1;
-      padding = 0;
-      hide_ascii = true;
-      separator = " ";
-      key_color = "Yellow";
-      separator_color = "Yellow";
-      palette = {
-        type = "Light";
-        glyph = " ● ";
-        visible = false;
-      };
-      bar = {
-        glyph = "●";
-        symbol_open = "(";
-        symbol_close = ")";
-        visible = false;
-        hide_delimiters = true;
-      };
-      box = {
-        title = " ";
-        border = "plain";
-        visible = false;
-        inner_margin = {
-          x = 2;
-          y = 1;
-        };
-      };
-      custom_ascii.color = "Yellow";
-      randomize = {
-        key_color = false;
-        separator_color = false;
-        pool = "base";
-      };
-    };
+  programs.macchina = {
+    enable = true;
     # https://github.com/Macchina-CLI/macchina/blob/main/macchina.toml
-    "macchina/macchina.toml".source = tomlFormat.generate "macchina-config" {
+    settings = {
       long_uptime = false;
       long_shell = false;
       long_kernel = false;
       current_shell = true;
       physical_cores = true;
-      theme = "custom";
+      theme = "Lithium";
+    };
+    themes = {
+      Lithium = "${config.programs.macchina.package.src}/contrib/themes/Lithium.toml";
     };
   };
 }
---
+++ home/options/macchina.nix
@@ -0,0 +1,95 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib)
+    mkEnableOption
+    mkPackageOption
+    mkOption
+    literalExpression
+    types
+    ;
+
+  tomlFormat = pkgs.formats.toml { };
+  cfg = config.programs.macchina;
+in
+{
+  meta.maintainers = with lib.maintainers; [ mirkolenz ];
+
+  options.programs.macchina = {
+    enable = mkEnableOption "macchina";
+
+    package = mkPackageOption pkgs "macchina" { };
+
+    settings = mkOption {
+      type = tomlFormat.type;
+      default = { };
+      example = literalExpression ''
+        {
+          interface = "wlan0";
+          long_uptime = true;
+          long_shell = false;
+          long_kernel = false;
+          current_shell = true;
+          physical_cores = true;
+        }
+      '';
+      description = ''
+        Configuration written to
+        {file}`$XDG_CONFIG_HOME/macchina/macchina.toml`.
+        See <https://github.com/Macchina-CLI/macchina/blob/main/macchina.toml>
+        for more information.
+      '';
+    };
+
+    themes = mkOption {
+      type = types.attrsOf (
+        types.oneOf [
+          tomlFormat.type
+          types.path
+          types.str
+        ]
+      );
+      default = { };
+      example = literalExpression ''
+        {
+          Lithium = "''${config.programs.macchina.package.src}/contrib/themes/Lithium.toml";
+        }
+      '';
+      description = ''
+        Configuration written to
+        {file}`$XDG_CONFIG_HOME/macchina/themes/*.toml`.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [ cfg.package ];
+
+    xdg.configFile = lib.mkMerge [
+      {
+        "macchina/macchina.toml" = lib.mkIf (cfg.settings != { }) {
+          source = tomlFormat.generate "macchina-config" cfg.settings;
+        };
+        # "macchina/themes" = lib.mkIf (cfg.themes != { }) {
+        #   source = pkgs.linkFarm "macchina-themes" (
+        #     lib.mapAttrsToList (name: value: {
+        #       name = "${name}.toml";
+        #       path = if lib.isAttrs value then tomlFormat.generate "macchina-theme-${name}" value else value;
+        #     }) cfg.themes
+        #   );
+        # };
+      }
+      (lib.mkIf (cfg.themes != { }) (
+        lib.mapAttrs' (name: value: {
+          name = "macchina/themes/${name}.toml";
+          value.source =
+            if lib.isAttrs value then tomlFormat.generate "macchina-theme-${name}" value else value;
+        }) cfg.themes
+      ))
+    ];
+  };
+}
