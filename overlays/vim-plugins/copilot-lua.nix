6895e59ceae2d9573a555ad315750dadfb3d64a4
Mirko Lenz <mirko@mirkolenz.com>
2025-04-08 09:54:13 +0200

    chore(overlays): update copilot lua

--- overlays/vim-plugins/copilot-lua.nix
+++ overlays/vim-plugins/copilot-lua.nix
@@ -1,12 +1,11 @@
 { vimUtils, fetchFromGitHub }:
 vimUtils.buildVimPlugin {
-  pname = "copilot.lua";
-  version = "0.0.0";
+  name = "copilot.lua";
   src = fetchFromGitHub {
     owner = "zbirenbaum";
     repo = "copilot.lua";
-    rev = "d661d65b4cab20a5c164f6d9081d91ed324fe4d8";
-    hash = "sha256-Sx5rasRT6IFx96jbFqzrpRblDQsVtaWnFv/swVnQGdI=";
+    rev = "1ff8ab7baae8dab4a9e078350624374cff0d5e71";
+    hash = "sha256-S5MnN+8Pjv5RDW9ZrS/3RMNgBWtiy9gbPd2xRoT27ME=";
   };
   meta.homepage = "https://github.com/zbirenbaum/copilot.lua/";
 }
