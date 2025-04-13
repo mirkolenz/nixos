993bfc3ae6c942bcd3849c19a1734cde5078c69e
Mirko Lenz <mirko@mirkolenz.com>
2025-04-13 16:05:27 +0200

    refactor(overlays): move mas to overrides

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -2,4 +2,11 @@
 final: prev: {
   inherit (final.unstable) nixos-rebuild-ng ltex-ls-plus jujutsu;
   inherit (final.stable) php;
+  mas = prev.mas.overrideAttrs (oldAttrs: rec {
+    version = "2.0.0";
+    src = prev.fetchurl {
+      url = "https://github.com/mas-cli/mas/releases/download/v${version}/mas-${version}.pkg";
+      hash = "sha256-/8w5cCUZF5jgmKTZHfevnBdNE3ChC759yV5WsZmGw6g=";
+    };
+  });
 }
