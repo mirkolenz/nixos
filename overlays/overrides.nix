d8cc6244b4165b1ec76b60fbd398f14afb4e6682
Mirko Lenz <mirko@mirkolenz.com>
2025-04-15 08:50:50 +0200

    chore(overlays): update mas

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -3,10 +3,10 @@ final: prev: {
   inherit (final.unstable) nixos-rebuild-ng ltex-ls-plus jujutsu;
   inherit (final.stable) php;
   mas = prev.mas.overrideAttrs (oldAttrs: rec {
-    version = "2.0.0";
+    version = "2.1.0";
     src = prev.fetchurl {
       url = "https://github.com/mas-cli/mas/releases/download/v${version}/mas-${version}.pkg";
-      hash = "sha256-/8w5cCUZF5jgmKTZHfevnBdNE3ChC759yV5WsZmGw6g=";
+      hash = "sha256-pT8W/ZdNP7Fv5nyTX9vKbTa2jIk3THN1HVCmuEIibfc=";
     };
   });
 }
