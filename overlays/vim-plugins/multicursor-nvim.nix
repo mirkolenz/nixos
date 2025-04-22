62fb6eb7b760a878c50575e6846e0fc64f6f423a
Mirko Lenz <mirko@mirkolenz.com>
2025-04-22 10:17:10 +0200

    chore(deps): update multicursor nvim

--- overlays/vim-plugins/multicursor-nvim.nix
+++ overlays/vim-plugins/multicursor-nvim.nix
@@ -4,8 +4,8 @@ vimUtils.buildVimPlugin {
   src = fetchFromGitHub {
     owner = "jake-stewart";
     repo = "multicursor.nvim";
-    rev = "f3a4899e5cdc93e6f8cd06bbc3b3631a2e85a315";
-    hash = "sha256-vJpXveXQvS25jxYSfBVifwANe43QCDHEgsD657pre9o=";
+    rev = "0ca2ccdec1f10430940f751a2044a0955777f174";
+    hash = "sha256-NcQszrisuVlcKkSOELD475NkaGFIx+m2ndBLQFMxhfI=";
   };
   meta.homepage = "https://github.com/jake-stewart/multicursor.nvim";
 }
