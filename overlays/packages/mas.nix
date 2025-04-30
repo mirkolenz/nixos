e869494282e54e3cd2f98c32b67a2c944c3d955f
Mirko Lenz <mirko@mirkolenz.com>
2025-04-30 12:27:50 +0200

    chore(overlays/mas): bump version

--- overlays/packages/mas.nix
+++ overlays/packages/mas.nix
@@ -1,8 +1,8 @@
 { nixpkgs, fetchurl }:
-nixpkgs.mas.overrideAttrs (oldAttrs: rec {
-  version = "2.2.1";
+nixpkgs.mas.overrideAttrs rec {
+  version = "2.2.2";
   src = fetchurl {
     url = "https://github.com/mas-cli/mas/releases/download/v${version}/mas-${version}.pkg";
-    hash = "sha256-Nk4rJP9vEZgQtveRBzN2eYNnVMCppVBuiCpXZ/PfNE8=";
+    hash = "sha256-v+tiD5ZMVFzeShyuOt8Ss3yw6p8VjopHaMimOQznL6o=";
   };
-})
+}
