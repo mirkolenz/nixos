113babdea0ef6a2fd91f28c56fe32d8a3b336c25
Mirko Lenz <mirko@mirkolenz.com>
2025-05-05 07:55:11 +0200

    chore(vim): use lsp keymaps from nixvim

--- vim/lsp/keymaps.nix
+++ vim/lsp/keymaps.nix
@@ -1,32 +1,23 @@
+{ ... }:
 {
-  lib',
-  ...
-}:
-{
-  keymaps =
-    lib'.self.mkVimKeymaps
-      {
-        prefix = "vim.lsp.";
-        raw = true;
-      }
-      [
-        {
-          key = "<leader>bf";
-          mode = [
-            "n"
-            "x"
-          ];
-          action = "buf.format()";
-          options.desc = "Format buffer";
-        }
-        {
-          key = "g.";
-          mode = [
-            "n"
-            "x"
-          ];
-          action = "buf.code_action()";
-          options.desc = "Code action";
-        }
+  lsp.keymaps = [
+    {
+      key = "<leader>bf";
+      mode = [
+        "n"
+        "x"
       ];
+      lspBufAction = "format";
+      options.desc = "Format buffer";
+    }
+    {
+      key = "g.";
+      mode = [
+        "n"
+        "x"
+      ];
+      lspBufAction = "code_action";
+      options.desc = "Code action";
+    }
+  ];
 }
