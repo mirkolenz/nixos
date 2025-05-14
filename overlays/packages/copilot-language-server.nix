6f314abc05846749e0028d7fdf900bb0390de7a1
Mirko Lenz <mirko@mirkolenz.com>
2025-05-14 09:24:46 +0200

    chore(deps): update copilot

--- overlays/packages/copilot-language-server.nix
+++ overlays/packages/copilot-language-server.nix
@@ -1,10 +1,10 @@
 # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/co/copilot-language-server/package.nix
 { nixpkgs, fetchzip }:
 nixpkgs.copilot-language-server.overrideAttrs (oldAttrs: {
-  version = "1.319.0";
+  version = "1.320.0";
   src = fetchzip {
     inherit (oldAttrs.src) url;
-    hash = "sha256-PN0MONsAeUAlRUeSxyRV6vg4cJmILpeaCw4yW1ZA5Mk=";
+    hash = "sha256-mRRg1KUpBdL134Rhtv/q/z7Ss6RKyb+HJYklIvNgo0o=";
     stripRoot = false;
   };
 })
