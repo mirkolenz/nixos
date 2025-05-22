d1ac50e797364c53ea06f5efcd233fe25c2a5650
Mirko Lenz <mirko@mirkolenz.com>
2025-05-22 08:14:37 +0200

    feat(home): enable zellij integration and update settings

--- home/mlenz/common/programs/zellij.nix
+++ home/mlenz/common/programs/zellij.nix
@@ -3,16 +3,19 @@
 
   programs.zellij = {
     enable = pkgs.stdenv.isLinux && (osConfig.services.openssh.enable or true);
-    enableBashIntegration = false;
-    enableFishIntegration = false;
-    enableZshIntegration = false;
-    # only works if shell integration is enabled
-    # attachExistingSession = true;
-    # exitShellOnExit = true;
+    # enableBashIntegration = false;
+    # enableFishIntegration = false;
+    # enableZshIntegration = false;
+    attachExistingSession = true; # requires shell integration
+    exitShellOnExit = true; # requires shell integration
     settings = {
       auto_layout = true;
       default_layout = "default";
+      default_mode = "normal";
       on_force_close = "detach";
+      show_release_notes = false;
+      show_startup_tips = false;
+      theme = "default";
     };
   };
 }
