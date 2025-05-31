e368935d7e3b0c721953793108ac033c6b56b34b
Mirko Lenz <mirko@mirkolenz.com>
2025-05-31 11:44:07 +0200

    fix(darwin): use nix 2.29

--- system/darwin/nix.nix
+++ system/darwin/nix.nix
@@ -1,6 +1,12 @@
-{ user, ... }:
+{
+  user,
+  lib,
+  pkgs,
+  ...
+}:
 {
   nix = {
+    package = lib.mkForce pkgs.nixVersions.nix_2_29;
     settings = {
       build-users-group = "nixbld";
       allowed-users = [ "@staff" ];
