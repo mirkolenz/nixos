4a76d9596bd5b01623c2aa1fe7fef433b87dcc70
Mirko Lenz <mirko@mirkolenz.com>
2025-06-04 07:39:12 +0200

    revert: enable sandbox
    This reverts commit dd64abf76f7ed1f9a0eb222a448eef4a7656da8d.

--- system/darwin/nix.nix
+++ system/darwin/nix.nix
@@ -11,6 +11,9 @@
       build-users-group = "nixbld";
       allowed-users = [ "@staff" ];
       trusted-users = [ user.login ];
+      # https://github.com/NixOS/nix/issues/4119#issuecomment-2561973914
+      extra-sandbox-paths = [ "/nix/store" ];
+      sandbox = "relaxed";
     };
     gc.interval = {
       Hour = 1;
--- system/linux/nix.nix
+++ system/linux/nix.nix
@@ -4,6 +4,7 @@
       # https://github.com/NixOS/nix/issues/7273#issuecomment-1310213986
       auto-optimise-store = true;
       allowed-users = [ "@wheel" ];
+      sandbox = true;
     };
     gc.dates = "daily";
   };
