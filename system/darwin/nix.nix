96c0c9ea7e653b23534c3188634f5e5fcc085f51
Mirko Lenz <mirko@mirkolenz.com>
2025-06-04 10:55:13 +0200

    fix(darwin/nix): remove extra sandbox paths

--- system/darwin/nix.nix
+++ system/darwin/nix.nix
@@ -11,8 +11,6 @@
       build-users-group = "nixbld";
       allowed-users = [ "@staff" ];
       trusted-users = [ user.login ];
-      # https://github.com/NixOS/nix/issues/4119#issuecomment-2561973914
-      extra-sandbox-paths = [ "/nix/store" ];
       sandbox = "relaxed";
     };
     gc.interval = {
