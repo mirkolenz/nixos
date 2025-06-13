379e754f68c0a7590a42a33186352788b6df04ab
Mirko Lenz <mirko@mirkolenz.com>
2025-06-13 08:06:01 +0200

    chore(home/claude): update settings

--- home/mlenz/common/programs/claude.nix
+++ home/mlenz/common/programs/claude.nix
@@ -2,6 +2,28 @@
 {
   programs.claude-code = {
     enable = config.custom.profile.isWorkstation;
+    settings = {
+      autoUpdaterStatus = "disabled";
+      cleanupPeriodDays = 30;
+      includeCoAuthoredBy = false;
+      model = "sonnet";
+      preferredNotifChannel = "terminal_bell";
+      theme = "dark";
+      verbose = false;
+      env = {
+        CLAUDE_BASH_MAINTAIN_PROJECT_WORKING_DIR = "1";
+      };
+      permissions = {
+        allow = [
+          "Edit"
+          "MultiEdit"
+          "NotebookEdit"
+          "WebSearch"
+          "Write"
+        ];
+        deny = [ ];
+      };
+    };
     guidance = ''
       I use `uv` to manage Python projects, so instead of `python` always use `uv run python`.
     '';
