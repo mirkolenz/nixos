8cefe2303a9ad38902591aef153ffb1a52099c00
Mirko Lenz <mirko@mirkolenz.com>
2025-06-16 17:23:32 +0200

    chore(deps): update goneovim

--- overlays/packages/goneovim-bin.nix
+++ overlays/packages/goneovim-bin.nix
@@ -9,8 +9,9 @@ let
 in
 mkApp rec {
   pname = "goneovim";
-  version = "0.6.13";
+  version = "0.6.14";
   appname = pname;
+  # prefetch-attrs .#goneovim-bin.passthru.urls --unpack
 
   passthru = {
     urls = {
@@ -18,8 +19,8 @@ mkApp rec {
       aarch64-darwin = "https://github.com/akiyosi/goneovim/releases/download/v${version}/${pname}-v${version}-macos-arm64.tar.bz2";
     };
     hashes = {
-      aarch64-darwin = "sha256-iLy3A0MgPCsKsc8sapMz5P2BkNw+d2cXTg7EKhWWybI=";
-      x86_64-darwin = "sha256-uuzYtsEDhIdhl6Mco5aQPbtSo9uaB0XQjmoq0CmFpIU=";
+      aarch64-darwin = "sha256-Kp/+7AIsbRgN0czeXsKnlCrHqsavV/caYaJZ6sgGvkw=";
+      x86_64-darwin = "sha256-d0kLzbLu0YQkdFQxGMlXPYfOZPUPUZS2HLMxNnW4CAc=";
     };
   };
 
