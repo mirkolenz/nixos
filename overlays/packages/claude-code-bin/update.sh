ada3cbdacc2bb29de2e6211fca2b9c43dd118909
Mirko Lenz <mirko@mirkolenz.com>
2025-07-12 10:52:51 +0200

    chore(deps): update claude-code-bin

--- overlays/packages/claude-code-bin/update.sh
+++ overlays/packages/claude-code-bin/update.sh
@@ -3,8 +3,7 @@
 
 set -euo pipefail
 
-# file="$(dirname "$BASH_SOURCE")/manifest.nix"
-file="overlays/packages/claude-code-bin/manifest.nix"
+file="$(dirname "$BASH_SOURCE")/manifest.nix"
 gcsBucket="https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases";
 version="$(curl -fsSL "$gcsBucket/stable")"
 
