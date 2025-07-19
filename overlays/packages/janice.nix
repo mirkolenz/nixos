f747b4377bf8891b6a216ced43755831f039b1e7
Mirko Lenz <mirko@mirkolenz.com>
2025-07-19 20:36:17 +0200

    chore(deps): update janice

--- overlays/packages/janice.nix
+++ overlays/packages/janice.nix
@@ -7,13 +7,13 @@
 }:
 buildGoModule rec {
   pname = "janice";
-  version = "0.9.0";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "ErikKalkoken";
     repo = "janice";
     tag = "v${version}";
-    hash = "sha256-SWs8HX8yzxFxU9wQbwoRU6JUGGySnvtV5iZbOcl7kl0=";
+    hash = "sha256-GkzxjsUkPw0hccsnxCLdr2D+AvFTdQ7+O4r9F0bUths=";
   };
 
   vendorHash = "sha256-rNL3eMWaT+Q+NoutUDvD9Gs9SM0B6s95P88ynsRp1GQ=";
