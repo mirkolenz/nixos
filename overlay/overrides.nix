71ed22480c5c74c0bad3b67637225a775cf2e65c
Mirko Lenz <mirko@mirkolenz.com>
2025-07-21 09:17:20 +0200

    chore(deps): update overrides

--- overlay/overrides.nix
+++ overlay/overrides.nix
@@ -1,4 +1,9 @@
 { ... }:
 final: prev: {
-  inherit (final.stable) git-annex bash-language-server;
+  inherit (final.stable)
+    # https://hydra.nixos.org/job/nixpkgs/trunk/git-annex.x86_64-darwin
+    git-annex
+    # https://github.com/nix-community/hydra-check/issues/79
+    hydra-check
+    ;
 }
