d7a03f6ab7fea27ca665e276bcfaa57d2b8b5b13
Mirko Lenz <mirko@mirkolenz.com>
2025-07-22 08:57:08 +0200

    chore(deps): remove hydra-check override

--- overlay/overrides.nix
+++ overlay/overrides.nix
@@ -3,7 +3,5 @@ final: prev: {
   inherit (final.stable)
     # https://hydra.nixos.org/job/nixpkgs/trunk/git-annex.x86_64-darwin
     git-annex
-    # https://github.com/nix-community/hydra-check/issues/79
-    hydra-check
     ;
 }
