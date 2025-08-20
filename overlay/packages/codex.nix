71752e9e002ab218b2e982607383547cf33f41e2
Mirko Lenz <mirko@mirkolenz.com>
2025-08-20 08:14:35 +0200

    chore(deps): update packages

--- overlay/packages/codex.nix
+++ overlay/packages/codex.nix
@@ -2,13 +2,13 @@
 { nixpkgs, rustPlatform }:
 nixpkgs.codex.overrideAttrs (
   finalAttrs: prevAttrs: {
-    version = "0.22.0";
+    version = "0.23.0";
     src = prevAttrs.src.override {
-      hash = "sha256-JTwtydW8LLBH/55+8a/BbqlZtkXsFKbT8dGoDEAjk1c=";
+      hash = "sha256-JS2nRh3/MNQ0mfdr2/Q10sAB38yWBLpw2zFf0dJORuM=";
     };
     cargoDeps = rustPlatform.fetchCargoVendor {
       inherit (finalAttrs) src sourceRoot;
-      hash = "sha256-3PljlyPfDsnjGmR/0iM7Fu1TnyDj31pKVcOU/izsL30=";
+      hash = "sha256-HbhOiTO7qFp64v+Bb62V1LxPH7qeTnWwkJKPEN4Vx6c=";
     };
     doCheck = false;
   }
