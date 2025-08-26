591bcf573f44e86a04dfdd8cf1579fdccf46461b
Mirko Lenz <mirko@mirkolenz.com>
2025-08-26 13:46:57 +0200

    fix(deps): update hotfixes

--- overlay/hotfixes.nix
+++ overlay/hotfixes.nix
@@ -1,3 +1,11 @@
 { ... }:
 final: prev: {
+  inherit (final.stable)
+    # https://hydra.nixos.org/job/nixpkgs/trunk/ffmpeg-normalize.x86_64-darwin
+    ffmpeg-normalize
+    # https://hydra.nixos.org/job/nixpkgs/trunk/gitui.x86_64-darwin
+    gitui
+    # https://hydra.nixos.org/job/nixpkgs/trunk/tectonic.x86_64-darwin
+    tectonic
+    ;
 }
