37b1207b6ed7d696305c61ccc1de8011b2f112bf
Mirko Lenz <mirko@mirkolenz.com>
2025-08-26 07:20:05 +0200

    chore(deps/caddy): update hash

--- overlay/packages/caddy.nix
+++ overlay/packages/caddy.nix
@@ -4,5 +4,5 @@ nixpkgs.caddy.withPlugins {
     # https://github.com/caddy-dns/cloudflare/tags
     "github.com/caddy-dns/cloudflare@v0.2.1"
   ];
-  hash = "sha256-saKJatiBZ4775IV2C5JLOmZ4BwHKFtRZan94aS5pO90=";
+  hash = "sha256-AcWko5513hO8I0lvbCLqVbM1eWegAhoM0J0qXoWL/vI=";
 }
