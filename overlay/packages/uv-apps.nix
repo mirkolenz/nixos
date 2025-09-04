cf078bb2592f78e7d041065e57cc2b787efc9799
Mirko Lenz <mirko@mirkolenz.com>
2025-09-04 09:14:41 +0200

    fix(uv-apps): improve derivation

--- overlay/packages/uv-apps.nix
+++ overlay/packages/uv-apps.nix
@@ -3,21 +3,26 @@
   symlinkJoin,
   writeShellApplication,
   uv-bin,
+  python3,
 }:
 let
   mkUvApp =
-    name: value:
+    name: values:
     writeShellApplication {
       inherit name;
       text = ''
-        exec ${lib.getExe' uv-bin "uvx"} ${value} "$@"
+        exec ${lib.getExe' uv-bin "uvx"} \
+          --python ${lib.getExe python3} \
+          ${toString (map (value: "--from ${value}") values)} \
+          ${name} "$@"
       '';
     };
 in
 symlinkJoin {
   name = "uv-apps";
   paths = lib.mapAttrsToList mkUvApp {
-    arguebuf = "arguebuf";
-    pyrefly = "pyrefly";
+    arguebuf = [ "arguebuf[cli]" ];
+    pyrefly = [ "pyrefly" ];
+    ast-grep-server = [ "git+https://github.com/ast-grep/ast-grep-mcp" ];
   };
 }
