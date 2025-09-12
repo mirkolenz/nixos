89abf5109b4d09bc99d1937adbdba4803fb83926
Mirko Lenz <mirko@mirkolenz.com>
2025-09-12 08:41:46 +0200

    chore(deps): remove hotfixes

--- overlay/hotfixes.nix
+++ overlay/hotfixes.nix
@@ -1,9 +1,3 @@
 { ... }:
 final: prev: {
-  inherit (final.stable)
-    # https://hydra.nixos.org/job/nixpkgs/trunk/ffmpeg-normalize.aarch64-darwin
-    ffmpeg-normalize
-    # https://hydra.nixos.org/job/nixpkgs/trunk/ttfautohint.aarch64-darwin
-    ttfautohint
-    ;
 }
