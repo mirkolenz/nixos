9a2d8efe095e5bb329c4ae3e04012701f47e9f02
Mirko Lenz <mirko@mirkolenz.com>
2025-09-12 11:10:01 +0200

    fix(deps): use stable buf

--- overlay/hotfixes.nix
+++ overlay/hotfixes.nix
@@ -2,4 +2,8 @@
 final: prev: {
   # https://github.com/NixOS/nixpkgs/issues/438765#issuecomment-3281041188
   tailscale = prev.tailscale.overrideAttrs { doCheck = false; };
+  inherit (final.stable)
+    # https://hydra.nixos.org/job/nixpkgs/trunk/buf.x86_64-darwin
+    buf
+    ;
 }
