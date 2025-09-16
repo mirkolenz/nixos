16934a09ae3bdfcc2bc57d734198573f836879d5
Mirko Lenz <mirko@mirkolenz.com>
2025-09-16 09:08:54 +0200

    chore(codex): update model settings

--- home/mlenz/common/channels/codex.nix
+++ home/mlenz/common/channels/codex.nix
@@ -11,8 +11,8 @@
     # https://github.com/openai/codex/blob/main/docs/config.md#config-reference
     settings = {
       model = "gpt-5-codex";
-      model_reasoning_effort = "high";
-      model_reasoning_summary = "auto";
+      # model_reasoning_effort = "high";
+      # model_reasoning_summary = "auto";
       # approval_policy = "never"; # User is never prompted
       approval_policy = "on-request"; # The model decides when to escalate
       file_opener = "none";
