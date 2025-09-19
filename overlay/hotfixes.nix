7750f5f0091c5beb4b2b81622df81f6a0771fae7
Mirko Lenz <mirko@mirkolenz.com>
2025-09-19 19:34:59 +0200

    chore(deps): update hotfixes

--- overlay/hotfixes.nix
+++ overlay/hotfixes.nix
@@ -1,11 +1,12 @@
 { ... }:
 final: prev:
-{
+{ }
+// (prev.lib.optionalAttrs (prev.stdenv.hostPlatform.system == "x86_64-darwin") {
+  ncdu = final.empty;
+
   inherit (final.stable)
     # https://hydra.nixos.org/job/nixpkgs/trunk/buf.x86_64-darwin
     buf
     ;
-}
-// (prev.lib.optionalAttrs (prev.stdenv.hostPlatform.system == "x86_64-darwin") {
-  ncdu = final.empty;
+
 })
