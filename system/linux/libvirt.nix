b1f6d74a0a7e643abe57ec80e4e76c993f6cee16
Mirko Lenz <mirko@mirkolenz.com>
2025-09-22 09:15:54 +0200

    fix(libvirt): use qemu kvm

--- system/linux/libvirt.nix
+++ system/linux/libvirt.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/virtualisation/libvirtd.nix
-  virtualisation.libvirtd.qemu.runAsRoot = false;
+  virtualisation.libvirtd = {
+    qemu.package = pkgs.qemu_kvm;
+    qemu.runAsRoot = false;
+  };
 
   # https://github.com/AshleyYakeley/NixVirt
   virtualisation.libvirt = {
