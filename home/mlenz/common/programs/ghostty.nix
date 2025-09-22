5832634d7aaf72f72a22fb9d06a6ff6f67279955
Mirko Lenz <mirko@mirkolenz.com>
2025-09-22 08:49:10 +0200

    chore(ghostty): update quick terminal size

--- home/mlenz/common/programs/ghostty.nix
+++ home/mlenz/common/programs/ghostty.nix
@@ -8,6 +8,7 @@
   programs.ghostty = lib.mkIf config.custom.profile.isDesktop {
     enable = true;
     package = if pkgs.stdenv.isDarwin then null else pkgs.ghostty;
+    # https://ghostty.org/docs/config/reference
     settings = {
       auto-update = "download";
       auto-update-channel = "stable";
@@ -16,7 +17,7 @@
       font-size = if pkgs.stdenv.isDarwin then 13 else 11;
       font-thicken = true;
       quick-terminal-position = "center";
-      quick-terminal-size = "50%,50%";
+      quick-terminal-size = "1000px,600px";
       shell-integration = "none";
       shell-integration-features = "no-cursor,sudo,title,ssh-env";
       theme = "light:GitHub Light Default,dark:GitHub Dark Default";
