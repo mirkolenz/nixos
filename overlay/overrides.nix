2dc706cc126b8221d247099851805174747e5d11
Mirko Lenz <mirko@mirkolenz.com>
2025-09-24 17:41:51 +0200

    fix(virt-manager): allow settings persistance on darwin

--- overlay/overrides.nix
+++ overlay/overrides.nix
@@ -1,4 +1,8 @@
-final: prev: {
+final: prev:
+let
+  lib = prev.lib;
+in
+{
   nixos-rebuild-ng =
     (prev.nixos-rebuild-ng.override {
       nix = final.determinate-nix;
@@ -18,4 +22,16 @@ final: prev: {
           githubActionsCheck = prev.stdenv.hostPlatform.system == "x86_64-linux";
         };
       });
+
+  # https://github.com/NixOS/nixpkgs/issues/294640
+  virt-manager = prev.virt-manager.overrideAttrs (oldAttrs: {
+    nativeBuildInputs =
+      (oldAttrs.nativeBuildInputs or [ ]) ++ (lib.optional prev.stdenv.isDarwin final.makeWrapper);
+    postInstall =
+      (oldAttrs.postInstall or "")
+      + (lib.optionalString prev.stdenv.isDarwin ''
+        wrapProgram $out/bin/virt-manager \
+          --set GSETTINGS_BACKEND keyfile
+      '');
+  });
 }
