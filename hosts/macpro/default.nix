5d7a26d018ee42400a7e470a13a1ac4015b68426
Mirko Lenz <mirko@mirkolenz.com>
2025-09-24 10:49:26 +0200

    feat(macpro): add network bridge

--- hosts/macpro/default.nix
+++ hosts/macpro/default.nix
@@ -47,10 +47,43 @@
   #   matchConfig.PermanentMACAddress = "40:6c:8f:b8:78:b3";
   #   linkConfig.Name = "ethernet1";
   # };
-  systemd.network.networks."20-failover" = {
-    matchConfig.Name = "enp10s0";
-    linkConfig.RequiredForOnline = false;
-    DHCP = "yes";
+  networking.useDHCP = false;
+  systemd.network = {
+    netdevs = {
+      "20-br0" = {
+        netdevConfig = {
+          Kind = "bridge";
+          Name = "br0";
+          MACAddress = "none";
+        };
+        bridgeConfig.STP = true;
+      };
+    };
+    links = {
+      "20-br0" = {
+        matchConfig.OriginalName = "br0";
+        linkConfig.MACAddressPolicy = "none";
+      };
+    };
+    networks = {
+      "20-enp9s0" = {
+        name = "enp9s0";
+        bridge = [ "br0" ];
+        DHCP = "no";
+        linkConfig.RequiredForOnline = true;
+      };
+      "20-enp10s0" = {
+        name = "enp10s0";
+        DHCP = "yes";
+        linkConfig.RequiredForOnline = false;
+      };
+      "20-br0" = {
+        name = "br0";
+        DHCP = "yes";
+        linkConfig.RequiredForOnline = "routable";
+        networkConfig.IPv6AcceptRA = true;
+      };
+    };
   };
 
   services.tailscale = {
