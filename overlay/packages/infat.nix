e7a1ec26a2b3b6f6f79a0343e48d93c4a925135b
Mirko Lenz <mirko@mirkolenz.com>
2025-09-27 15:00:02 +0200

    chore(infat): use finalAttrs pattern

--- overlay/packages/infat.nix
+++ overlay/packages/infat.nix
@@ -5,14 +5,14 @@
   versionCheckHook,
   nix-update-script,
 }:
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "infat";
   version = "3.0.1";
 
   src = fetchFromGitHub {
     owner = "philocalyst";
     repo = "infat";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-o8Ga9Wd5wIIjtA/dse7aUCHw2cYgwvUKVzi0PaDNQGw=";
   };
 
@@ -27,11 +27,11 @@ rustPlatform.buildRustPackage rec {
   meta = {
     description = "A command line tool to set default openers for file formats and url schemes on macos";
     homepage = "https://github.com/philocalyst/infat";
-    changelog = "https://github.com/philocalyst/infat/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/philocalyst/infat/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.mit;
     platforms = lib.platforms.darwin;
     maintainers = with lib.maintainers; [ mirkolenz ];
     mainProgram = "infat";
     githubActionsCheck = true;
   };
-}
+})
