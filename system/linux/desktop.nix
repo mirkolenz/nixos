c0ac69ebb0928b7554159b1aa1a4aafef4ea76d9
Mirko Lenz <mirko@mirkolenz.com>
2025-09-27 13:21:48 +0200

    refactor(linux): merge desktop and xserver modules

--- system/linux/desktop.nix
+++ system/linux/desktop.nix
@@ -2,24 +2,46 @@
   lib,
   config,
   user,
+  pkgs,
   ...
 }:
 lib.mkIf config.custom.profile.isDesktop {
-  services.xserver.enable = true;
-
-  security.rtkit.enable = config.services.pipewire.enable;
-  services.pipewire = {
-    enable = true;
-    alsa = {
+  services = {
+    desktopManager.cosmic.enable = true;
+    displayManager.cosmic-greeter.enable = true;
+    xserver = {
       enable = true;
-      support32Bit = true;
+      xkb.layout = "us";
+      excludePackages = with pkgs; [ xterm ];
+    };
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
     };
   };
+  # Packages
+  environment = {
+    sessionVariables.NIXOS_OZONE_WL = "1";
+    systemPackages = with pkgs; [
+      anydesk
+      google-chrome
+      firefox
+      obsidian
+      zoom-us
+      zotero
+    ];
+  };
+
+  # there is an issue with wpa_supplicant and broadcom-wl (used in Macs)
+  networking.networkmanager.wifi.backend = "iwd";
+
+  security.rtkit.enable = config.services.pipewire.enable;
 
   hardware.graphics.enable = true;
 
-  systemd.network.wait-online.anyInterface = true;
-  boot.initrd.systemd.network.wait-online.anyInterface = true;
   networking.networkmanager.enable = true;
   users.users.${user.login}.extraGroups = [ "networkmanager" ];
 
