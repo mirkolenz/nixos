0aabfc9d43a79ead1f3e9aed74686f5402747f18
Mirko Lenz <mirko@mirkolenz.com>
2025-09-27 15:35:42 +0200

    fix(home): remove nixpkgs session variables

--- home/mlenz/common/default.nix
+++ home/mlenz/common/default.nix
@@ -14,8 +14,5 @@
     stateVersion = stateVersions.home;
     username = user.login;
     homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${user.login}" else "/home/${user.login}";
-    sessionVariables = {
-      NIXPKGS_ALLOW_UNFREE = "1";
-    };
   };
 }
