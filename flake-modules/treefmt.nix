cb999d139d07fa3647bd5630ea2391ad30240599
Mirko Lenz <mirko@mirkolenz.com>
2025-10-04 17:49:10 +0200

    chore(treemft): update programs

--- flake-modules/treefmt.nix
+++ flake-modules/treefmt.nix
@@ -7,11 +7,16 @@
       treefmt = {
         projectRootFile = "flake.nix";
         programs = {
+          buf.enable = true;
+          gofmt.enable = true;
+          google-java-format.enable = true;
           nixfmt.enable = true;
           prettier.enable = true;
           ruff-check.enable = true;
           ruff-format.enable = true;
+          rustfmt.enable = true;
           texfmt.enable = true;
+          typstyle.enable = true;
         };
       };
       packages.treefmt-nix = config.treefmt.build.wrapper;
