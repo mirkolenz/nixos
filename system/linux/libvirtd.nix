e5f564375d50af970a59e1eeae527cc85b1e8a3c
Mirko Lenz <mirko@mirkolenz.com>
2025-10-04 17:34:42 +0200

    refactor(libvirtd): simplify config

--- system/linux/libvirtd.nix
+++ system/linux/libvirtd.nix
@@ -12,10 +12,7 @@
       package = pkgs.qemu_kvm;
       runAsRoot = false;
       vhostUserPackages = [ pkgs.virtiofsd ];
-      swtpm = {
-        enable = true;
-        package = pkgs.swtpm;
-      };
+      swtpm.enable = true;
     };
   };
 
@@ -25,13 +22,9 @@
 
   programs.virt-manager.enable = lib.mkDefault config.custom.profile.isDesktop;
 
-  environment.systemPackages =
-    (lib.optionals config.virtualisation.libvirtd.enable [
-      pkgs.dnsmasq
-    ])
-    ++ (lib.optionals config.custom.profile.isDesktop [
-      pkgs.virt-viewer
-    ]);
+  environment.systemPackages = lib.mkIf config.custom.profile.isDesktop [
+    pkgs.virt-viewer
+  ];
 
   environment.etc = lib.mkIf config.virtualisation.libvirtd.enable {
     "nix-libvirtd/images/virtio-win.iso".source = pkgs.virtio-win.src;
