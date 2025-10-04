c4b7ddada56ea7d690c670e9282d60d275f66caf
Mirko Lenz <mirko@mirkolenz.com>
2025-10-04 22:00:34 +0200

    refactor(orbstack): update config

--- hosts/orbstack/default.nix
+++ hosts/orbstack/default.nix
@@ -2,7 +2,6 @@
   modulesPath,
   user,
   lib,
-  lib',
   ...
 }:
 let
@@ -28,50 +27,4 @@ in
       source = zedSettings;
     };
   };
-
-  # /etc/nixos/configuration.nix
-  security.sudo.wheelNeedsPassword = false;
-
-  # This being `true` leads to a few nasty bugs, change at your own risk!
-  users.mutableUsers = lib.mkForce false;
-
-  networking = {
-    dhcpcd.enable = false;
-    useDHCP = false;
-    useHostResolvConf = false;
-  };
-
-  systemd.network = {
-    enable = true;
-    networks."50-eth0" = {
-      name = "eth0";
-      networkConfig = {
-        DHCP = "ipv4";
-        IPv6AcceptRA = true;
-      };
-      linkConfig.RequiredForOnline = "routable";
-    };
-  };
-
-  users.groups.orbstack.gid = 67278;
-
-  users.users.${user.login} = {
-    uid = 501;
-    extraGroups = [
-      "wheel"
-      "orbstack"
-    ];
-
-    # simulate isNormalUser, but UID < 1000
-    isNormalUser = lib.mkForce false;
-    isSystemUser = true;
-    group = lib.mkForce "users";
-    createHome = true;
-    home = "/home/mlenz";
-    homeMode = "700";
-    useDefaultShell = true;
-  };
-
-  # Extra certificates from OrbStack.
-  security.pki.certificateFiles = lib'.flocken.optionalPath /opt/orbstack-guest/run/extra-certs.crt;
 }
--- hosts/orbstack/hardware.nix
+++ hosts/orbstack/hardware.nix
@@ -1,6 +1,58 @@
-# /etc/nixos/orbstack.nix
-{ lib, config, ... }:
 {
+  lib,
+  lib',
+  config,
+  user,
+  ...
+}:
+{
+
+  # /etc/nixos/configuration.nix
+  security.sudo.wheelNeedsPassword = false;
+
+  # This being `true` leads to a few nasty bugs, change at your own risk!
+  users.mutableUsers = lib.mkForce false;
+
+  networking = {
+    dhcpcd.enable = false;
+    useDHCP = false;
+    useHostResolvConf = false;
+  };
+
+  systemd.network = {
+    enable = true;
+    networks."50-eth0" = {
+      name = "eth0";
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
+
+  users.users.${user.login} = {
+    uid = 501;
+    extraGroups = [
+      "wheel"
+      "orbstack"
+    ];
+
+    # simulate isNormalUser, but UID < 1000
+    isNormalUser = lib.mkForce false;
+    isSystemUser = true;
+    group = lib.mkForce "users";
+    createHome = true;
+    home = "/home/mlenz";
+    homeMode = "700";
+    useDefaultShell = true;
+  };
+
+  # Extra certificates from OrbStack.
+  security.pki.certificateFiles = lib'.flocken.optionalPath /opt/orbstack-guest/run/extra-certs.crt;
+
+  # /etc/nixos/orbstack.nix
+
   # Add OrbStack CLI tools to PATH
   environment.shellInit = ''
     . /opt/orbstack-guest/etc/profile-early
@@ -54,4 +106,6 @@
     "x86_64-linux"
     "i686-linux"
   ];
+
+  users.groups.orbstack.gid = 67278;
 }
