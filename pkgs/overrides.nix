717338a42cf7fd6674d4f61a5c1d31eb41241782
Mirko Lenz <mirko@mirkolenz.com>
2025-10-09 09:11:31 +0200

    fix(pkgs): update overrides

--- pkgs/overrides.nix
+++ pkgs/overrides.nix
@@ -1,5 +1,15 @@
 final: prev:
 { }
+// (prev.lib.optionalAttrs prev.stdenv.isDarwin {
+  inherit (final.stable)
+    # https://hydra.nixos.org/job/nixpkgs/trunk/basedpyright.aarch64-darwin
+    basedpyright
+    # https://hydra.nixos.org/job/nixpkgs/trunk/virt-manager.aarch64-darwin
+    vte
+    # https://hydra.nixos.org/job/nixpkgs/trunk/time.aarch64-darwin
+    time
+    ;
+})
 // (prev.lib.optionalAttrs (prev.stdenv.hostPlatform.system == "x86_64-darwin") {
   ncdu = final.empty;
 })
