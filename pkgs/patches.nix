8ef98a95b01ce1be9ab2de41a6fa83c8d50b965d
Mirko Lenz <mirko@mirkolenz.com>
2025-10-11 21:20:19 +0200

    fix(pkgs): update patches

--- pkgs/patches.nix
+++ pkgs/patches.nix
@@ -7,10 +7,6 @@ final: prev:
 }
 // (prev.lib.optionalAttrs prev.stdenv.isDarwin {
   inherit (final.stable)
-    # https://hydra.nixos.org/job/nixpkgs/trunk/basedpyright.aarch64-darwin
-    basedpyright
-    # https://hydra.nixos.org/job/nixpkgs/trunk/virt-manager.aarch64-darwin
-    vte
     # https://hydra.nixos.org/job/nixpkgs/trunk/time.aarch64-darwin
     time
     ;
