905f66169295b714e5b9af6bf8d23bb00ead8cfe
Mirko Lenz <mlenz@mirkos-macbook.taildc4a8b.ts.net>
2025-10-24 21:30:41 +0200

    fix(deps): use stable gitui

--- pkgs/hotfixes.nix
+++ pkgs/hotfixes.nix
@@ -3,6 +3,8 @@ final: prev:
   inherit (final.stable)
     # https://hydra.nixos.org/job/nixpkgs/trunk/libraspberrypi.aarch64-linux
     libraspberrypi
+    # https://hydra.nixos.org/job/nixpkgs/trunk/gitui.aarch64-darwin
+    gitui
     ;
 }
 // (prev.lib.optionalAttrs prev.stdenv.isDarwin {
