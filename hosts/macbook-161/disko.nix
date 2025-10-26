e38794798ef8620632c514d21fc490bc0fe887b2
Mirko Lenz <mirko@mirkolenz.com>
2025-10-26 21:11:26 +0100

    fix(hosts/macbook-161): add luks encryption

--- hosts/macbook-161/disko.nix
+++ hosts/macbook-161/disko.nix
@@ -1,48 +1,48 @@
 # https://github.com/nix-community/disko/blob/master/example/luks-btrfs-subvolumes.nix
 # https://github.com/nix-community/disko/blob/master/example/btrfs-subvolumes.nix
 {
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/disk/by-diskseq/1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/root" = {
-                    mountpoint = "/";
-                  };
-                  "/home" = {
-                    mountpoint = "/home";
-                    mountOptions = [ "compress=zstd" ];
-                  };
-                  "/nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                  };
-                  # "/swap" = {
-                  #   mountpoint = "/.swapvol";
-                  #   swap.swapfile.size = "4G";
-                  # };
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/disk/by-diskseq/1";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "luks";
+            passwordFile = "/tmp/secret.key"; # Interactive
+            # settings.keyFile = "/tmp/secret.key"; # Non-interactive
+            settings.allowDiscards = true;
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                };
+                "/home" = {
+                  mountpoint = "/home";
+                  mountOptions = [
+                    "compress=zstd"
+                  ];
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
                 };
               };
             };
