91bbbea9d0b3f9b6c1daf6d865509d2a372e0dca
Mirko Lenz <mirko@mirkolenz.com>
2025-11-18 09:10:39 +0100

    ci: build all configs

--- flake-modules/checks.nix
+++ flake-modules/checks.nix
@@ -6,4 +6,32 @@
     ) self.darwinConfigurations;
     homeConfigurations = lib.mapAttrs (name: module: module.activationPackage) self.homeConfigurations;
   };
+  perSystem =
+    { system, ... }:
+    let
+      currentSystem = name: value: value.pkgs.stdenv.hostPlatform.system == system;
+
+      nixosConfigs = lib.mapAttrs' (name: value: {
+        name = "nixos-config-${name}";
+        value = value.config.system.build.toplevel;
+      }) (lib.filterAttrs currentSystem self.nixosConfigurations);
+
+      darwinConfigs = lib.mapAttrs' (name: value: {
+        name = "darwin-config-${name}";
+        value = value.config.system.build.toplevel;
+      }) (lib.filterAttrs currentSystem self.darwinConfigurations);
+
+      homeConfigs = lib.mapAttrs' (name: value: {
+        name = "home-config-${name}";
+        value = value.activationPackage;
+      }) (lib.filterAttrs currentSystem self.homeConfigurations);
+
+    in
+    {
+      packages = lib.mkMerge [
+        nixosConfigs
+        darwinConfigs
+        homeConfigs
+      ];
+    };
 }
