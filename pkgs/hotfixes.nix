263d034b77560aa871ba49be01406508f8b5dbab
Mirko Lenz <mirko@mirkolenz.com>
2025-11-21 08:21:58 +0100

    fix(pkgs): remove tealdeer hotfix

--- pkgs/hotfixes.nix
+++ pkgs/hotfixes.nix
@@ -3,10 +3,6 @@ final: prev:
 // (prev.lib.optionalAttrs prev.stdenv.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.isDarwin {
-  inherit (final.stable)
-    # https://hydra.nixos.org/job/nixpkgs/trunk/tealdeer.aarch64-darwin
-    tealdeer
-    ;
 })
 // (prev.lib.optionalAttrs (prev.stdenv.hostPlatform.system == "x86_64-darwin") {
   container = final.empty;
