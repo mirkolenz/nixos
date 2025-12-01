f5020b0b2c6bc8f33470c960eb35022256fecbab
Mirko Lenz <mirko@mirkolenz.com>
2025-12-01 09:59:49 +0100

    fix(linux): update firewall definition

--- system/linux/network.nix
+++ system/linux/network.nix
@@ -2,11 +2,18 @@
 {
   networking = {
     useNetworkd = true;
-    firewall.enable = false;
     # this is overridden by NetworkManager on workstations
     useDHCP = lib.mkDefault true;
     # this is not compatible with networkd
     useHostResolvConf = false;
+    # firewall currently disabled as not completely configured
+    firewall = {
+      enable = false;
+      trustedInterfaces = [
+        # libvirt uses iptables directly
+        "virbr0"
+      ];
+    };
   };
 
   services.firewalld.enable = config.networking.firewall.enable;
