5d65742179be1deabcffa3538b1375b9aea1efed
Mirko Lenz <mirko@mirkolenz.com>
2025-12-06 21:58:12 +0100

    feat(system/linux): enable rssh pam

--- system/linux/ssh.nix
+++ system/linux/ssh.nix
@@ -1,5 +1,12 @@
-{ ... }:
+{ config, user, ... }:
 {
+  security.pam = {
+    rssh = {
+      inherit (config.services.openssh) enable;
+      settings.auth_key_file = "/etc/ssh/authorized_keys.d/${user.login}";
+    };
+    services.sudo.rssh = config.services.openssh.enable && config.security.pam.rssh.enable;
+  };
   services.openssh = {
     settings = {
       PasswordAuthentication = false;
