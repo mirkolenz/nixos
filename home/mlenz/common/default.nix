19a2cdd22c9ff437f6d4cd543c1fd4f0f5cab1c9
Mirko Lenz <mirko@mirkolenz.com>
2025-12-10 23:00:18 +0100

    fix(home): add visual env var

--- home/mlenz/common/default.nix
+++ home/mlenz/common/default.nix
@@ -3,6 +3,7 @@
   lib',
   user,
   stateVersions,
+  config,
   ...
 }:
 {
@@ -14,5 +15,8 @@
     stateVersion = stateVersions.home;
     username = user.login;
     homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${user.login}" else "/home/${user.login}";
+    sessionVariables = {
+      VISUAL = config.home.sessionVariables.EDITOR;
+    };
   };
 }
