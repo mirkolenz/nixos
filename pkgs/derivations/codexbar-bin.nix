063f54ad47d6bcc6df0fbe9755a71e1c8f6ecb3c
Mirko Lenz <mirko@mirkolenz.com>
2025-12-19 07:57:34 +0100

    chore(deps): update pkgs

--- pkgs/derivations/codexbar-bin.nix
+++ pkgs/derivations/codexbar-bin.nix
@@ -7,12 +7,12 @@
 }:
 mkApp (finalAttrs: {
   pname = "codexbar";
-  version = "0.8.1";
+  version = "0.9.0";
   appname = "CodexBar";
 
   src = fetchurl {
     url = "https://github.com/steipete/CodexBar/releases/download/v${finalAttrs.version}/CodexBar-${finalAttrs.version}.zip";
-    hash = "sha256-g/CkFoSzryxEsxHHLlNC7EqG9dYpclNiozBfL8C6UjU=";
+    hash = "sha256-KClkd9fRThJN8STFCAQG/9rK+IdFzAI9xenG84oORBM=";
   };
 
   nativeBuildInputs = [ unzip ];
