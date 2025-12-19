f191d4296c4292f83e4fb5f39e815ac9edb8a58c
Mirko Lenz <mirko@mirkolenz.com>
2025-12-19 10:10:21 +0100

    chore(pkgs): add font-awesome-web

---
+++ pkgs/derivations/font-awesome-web.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "font-awesome-web";
+  version = "7.1.0";
+
+  src = fetchFromGitHub {
+    owner = "FortAwesome";
+    repo = "Font-Awesome";
+    tag = finalAttrs.version;
+    hash = "sha256-1hHjqPXz/+otqgRsfXQG4D3uAmKOzXo2eBqOUzSU8IM=";
+  };
+
+  dontConfigure = true;
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share
+    cp -r css webfonts $out/share/
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Font Awesome web assets (CSS and webfonts)";
+    homepage = "https://github.com/FortAwesome/Font-Awesome";
+    license = lib.licenses.ofl;
+    maintainers = with lib.maintainers; [ mirkolenz ];
+    platforms = lib.platforms.all;
+  };
+})
