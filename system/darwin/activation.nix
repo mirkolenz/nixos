a42f476b0d10be000d9a8438d94e62a3596128e1
Mirko Lenz <mirko@mirkolenz.com>
2025-12-20 10:45:39 +0100

    fix(system/darwin): make determinate nixd upgrade deterministic

--- system/darwin/activation.nix
+++ system/darwin/activation.nix
@@ -1,11 +1,18 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   # https://github.com/nix-darwin/nix-darwin/blob/master/modules/system/activation-scripts.nix
   system.activationScripts = {
     preActivation.text = /* bash */ '''';
     extraActivation.text = /* bash */ '''';
     postActivation.text = /* bash */ ''
-      /usr/local/bin/determinate-nixd upgrade
+      expected_version="${pkgs.determinate-nix.version}"
+      current_version=$(/usr/local/bin/determinate-nixd version | ${lib.getExe pkgs.gawk} '/daemon version:/ {print $NF; exit}')
+      if [ "$current_version" != "$expected_version" ]; then
+        echo "determinate-nixd: upgrading from $current_version to $expected_version"
+        /usr/local/bin/determinate-nixd upgrade --version "v$expected_version"
+      else
+        echo "determinate-nixd: already at version $expected_version"
+      fi
     '';
   };
 }
