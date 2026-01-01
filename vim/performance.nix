fec77f6bfd2deee19fa8498fba21fedc7bf71a99
Mirko Lenz <mirko@mirkolenz.com>
2026-01-01 20:26:52 +0100

    chore(vim/performance): disable

--- vim/performance.nix
+++ vim/performance.nix
@@ -1,16 +1,18 @@
 { ... }:
 {
   performance = {
+    # causes increased build time in ci
     byteCompileLua = {
-      enable = true;
+      enable = false;
       configs = true;
       initLua = true;
       luaLib = true;
       nvimRuntime = true;
       plugins = true;
     };
+    # fails due to conflicts with nvim-treesitter-grammars
     combinePlugins = {
-      enable = true;
+      enable = false;
       standalonePlugins = [
         "blink.cmp"
         "nvim-treesitter"
