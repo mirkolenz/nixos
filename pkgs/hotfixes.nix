6235e49d7bf4abc75f5c3c095bd7ce4f6b6532c0
Mirko Lenz <mirko@mirkolenz.com>
2026-01-01 20:49:57 +0100

    chore(pkgs): update hotfixes

--- pkgs/hotfixes.nix
+++ pkgs/hotfixes.nix
@@ -1,5 +1,12 @@
 final: prev:
-{ }
+{
+  inherit (final.stable)
+    # https://hydra.nixos.org/job/nixpkgs/trunk/llm.aarch64-darwin
+    llm
+    # https://hydra.nixos.org/job/nixpkgs/trunk/mcp-proxy.aarch64-darwin
+    mcp-proxy
+    ;
+}
 // (prev.lib.optionalAttrs prev.stdenv.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.isDarwin {
