f8cc5e09e26e8eb52963ae133103ebb8358f4458
Mirko Lenz <mirko@mirkolenz.com>
2026-01-15 16:15:40 +0100

    fix(system): simplify shell handling

--- system/common/shells.nix
+++ system/common/shells.nix
@@ -1,18 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs = {
-    # bash is enabled by default
-    fish = {
-      enable = true;
-    };
-    zsh = {
-      enable = true;
-    };
+    bash.enable = true;
+    fish.enable = true;
+    zsh.enable = true;
   };
-  environment.shells = with pkgs; [ fish ];
-  environment.pathsToLink = [
-    "/share/bash-completion"
-    "/share/fish"
-    "/share/zsh"
-  ];
 }
