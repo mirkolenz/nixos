86b4aa9bab088df1086997fe0f4c44d5b56a6cbd
Mirko Lenz <mirko@mirkolenz.com>
2026-01-25 20:09:28 +0100

    fix(pkgs): update hotfixes

--- pkgs/hotfixes.nix
+++ pkgs/hotfixes.nix
@@ -4,4 +4,9 @@ final: prev:
 // (prev.lib.optionalAttrs prev.stdenv.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.isDarwin {
+  inherit (final.stable)
+    # https://hydra.nixos.org/job/nixpkgs/unstable/swift.aarch64-darwin
+    pam-watchid
+    pre-commit
+    ;
 })
