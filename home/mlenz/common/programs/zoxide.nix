a203e6bc5be2f2f2cf731d75714b4ab37adf2d18
Mirko Lenz <mirko@mirkolenz.com>
2026-01-28 08:12:09 +0100

    chore(home/zoxide): update config

--- home/mlenz/common/programs/zoxide.nix
+++ home/mlenz/common/programs/zoxide.nix
@@ -1,7 +1,14 @@
 { lib, ... }:
 {
+  # https://github.com/ajeetdsouza/zoxide#configuration
   programs.zoxide = {
     enable = true;
+    options = [
+      "--cmd"
+      "z"
+      "--hook"
+      "prompt"
+    ];
   };
   home.sessionVariables = {
     _ZO_ECHO = "1";
@@ -13,5 +20,7 @@
       "/nix/**"
       "/tmp/**"
     ];
+    _ZO_MAXAGE = "10000";
+    _ZO_RESOLVE_SYMLINKS = "1";
   };
 }
