632e8779285fcdddb9883dfa1a17fdbf953af640
Mirko Lenz <mirko@mirkolenz.com>
2026-02-06 15:18:49 +0100

    fix(pkgs/codexbar): update and add wrapper

--- pkgs/derivations/codexbar-bin.nix
+++ pkgs/derivations/codexbar-bin.nix
@@ -7,19 +7,21 @@
 }:
 mkApp (finalAttrs: {
   pname = "codexbar";
-  version = "0.17.0";
+  version = "0.18.0-beta.2";
   appname = "CodexBar";
 
   src = fetchurl {
     url = "https://github.com/steipete/CodexBar/releases/download/v${finalAttrs.version}/CodexBar-${finalAttrs.version}.zip";
-    hash = "sha256-LD86SQCPLi/yZOSjatAntcFA1hwzGsLnrOLB/5sMadw=";
+    hash = "sha256-rsecyS+F8rM644Lfp43fheQm3Q2Z7lKS3VsH+hdifyA=";
   };
 
   nativeBuildInputs = [ unzip ];
   sourceRoot = ".";
 
+  wrapperPath = "Contents/Helpers/CodexBarCLI";
+
   passthru.updateScript = nix-update-script {
-    extraArgs = [ "--url=${finalAttrs.meta.homepage}" ];
+    extraArgs = [ "--version=unstable" ];
   };
 
   meta = {
