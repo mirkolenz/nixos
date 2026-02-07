f2cce1d5cefd40076d51b0b7558aae0515b1dea0
Mirko Lenz <mirko@mirkolenz.com>
2026-02-07 21:00:09 +0100

    chore(pkgs): update hotfixes

--- pkgs/hotfixes.nix
+++ pkgs/hotfixes.nix
@@ -4,11 +4,8 @@ final: prev:
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin {
-  # https://hydra.nixos.org/job/nixpkgs/unstable/swift.aarch64-darwin
   inherit (final.stable)
-    # https://hydra.nixos.org/job/nixpkgs/unstable/pam-watchid.aarch64-darwin
-    pam-watchid
-    # https://hydra.nixos.org/job/nixpkgs/unstable/pre-commit.aarch64-darwin
-    pre-commit
+    # https://hydra.nixos.org/job/nixpkgs/unstable/yt-dlp.aarch64-darwin
+    yt-dlp
     ;
 })
