99d93647626d9f1f5719205f4ef7429d506563ba
Mirko Lenz <mirko@mirkolenz.com>
2026-02-08 10:27:15 +0100

    chore(claude): update sandbox settings

--- home/mlenz/common/programs/claude.nix
+++ home/mlenz/common/programs/claude.nix
@@ -5,7 +5,7 @@
   ...
 }:
 lib.mkIf config.custom.profile.isWorkstation {
-  # https://code.claude.com/docs/en/iam
+  # https://code.claude.com/docs/en/permissions
   # https://code.claude.com/docs/en/sandboxing
   # https://code.claude.com/docs/en/settings
   programs.claude-code = {
@@ -21,6 +21,7 @@ lib.mkIf config.custom.profile.isWorkstation {
       sandbox = {
         enabled = true;
         excludedCommands = [ "nix" ];
+        # todo: does not work as of 2026-02-08
         network.allowUnixSockets = [ "/nix/var/nix/daemon-socket/socket" ];
       };
       env = {
@@ -35,18 +36,20 @@ lib.mkIf config.custom.profile.isWorkstation {
       permissions = {
         defaultMode = "acceptEdits";
         disableBypassPermissionsMode = "disable";
+        # absolute paths need // prefix, otherwise they are treated as relative to the project root
         allow = [
           "WebFetch"
           "WebSearch"
-          "Edit(${config.xdg.cacheHome}/**)"
-          "Edit(${config.home.homeDirectory}/.npm/**)"
-          # "Bash(nix fmt)"
-          # "Bash(nix build:*)"
+          "Edit(/${config.xdg.cacheHome}/**)"
+          "Edit(/${config.home.homeDirectory}/.npm/**)"
+          "Bash(nix *)"
+          "Bash(uv run pytest *)" # calls stat() which conflicts with .env deny
         ];
         deny = [
-          "Bash(sudo:*)"
+          "Bash(sudo *)"
           "Read(.env*)"
           "Read(*secret*)"
+          "Bash(nix run *)"
         ];
         ask = [ ];
       };
