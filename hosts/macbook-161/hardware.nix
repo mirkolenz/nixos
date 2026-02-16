516961777d3e257ea7c3671b28b2d7bb2f353771
Mirko Lenz <mirko@mirkolenz.com>
2026-02-16 17:11:24 +0100

    fix(hosts/macbook-161): update kernel modules

--- hosts/macbook-161/hardware.nix
+++ hosts/macbook-161/hardware.nix
@@ -8,7 +8,7 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
   boot.initrd.availableKernelModules = [
     "xhci_pci"
-    "ahci"
+    "nvme"
     "usbhid"
     "usb_storage"
     "sd_mod"
