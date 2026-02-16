204103a9b4c0843376d3fb09f7027940a0a3eafe
Mirko Lenz <mirko@mirkolenz.com>
2026-02-16 12:14:30 +0100

    fix(hosts/macbook-161): correctly declare disko

--- hosts/macbook-161/disko.nix
+++ hosts/macbook-161/disko.nix
@@ -3,37 +3,40 @@
 {
   # Only manages the Linux partition (nvme0n1p3)
   # Does NOT touch macOS EFI (nvme0n1p1) or APFS (nvme0n1p2) partitions
-  disko.devices.luks.root = {
-    type = "luks";
+  disko.devices.disk.main = {
+    type = "disk";
     device = "/dev/disk/by-id/nvme-APPLE_SSD_AP1024N_C02001700E3N9V014-part3";
-    name = "cryptroot";
-    passwordFile = "/tmp/secret.key"; # Interactive
-    # settings.keyFile = "/tmp/secret.key"; # Non-interactive
-    settings.allowDiscards = true;
+    destroy = false;
     content = {
-      type = "btrfs";
-      extraArgs = [ "-f" ];
-      subvolumes = {
-        "/root" = {
-          mountpoint = "/";
-          mountOptions = [
-            "compress=zstd"
-            "noatime"
-          ];
-        };
-        "/home" = {
-          mountpoint = "/home";
-          mountOptions = [
-            "compress=zstd"
-            "noatime"
-          ];
-        };
-        "/nix" = {
-          mountpoint = "/nix";
-          mountOptions = [
-            "compress=zstd"
-            "noatime"
-          ];
+      type = "luks";
+      name = "cryptroot";
+      passwordFile = "/tmp/secret.key"; # Interactive
+      # settings.keyFile = "/tmp/secret.key"; # Non-interactive
+      settings.allowDiscards = true;
+      content = {
+        type = "btrfs";
+        subvolumes = {
+          "/root" = {
+            mountpoint = "/";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+            ];
+          };
+          "/home" = {
+            mountpoint = "/home";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+            ];
+          };
+          "/nix" = {
+            mountpoint = "/nix";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+            ];
+          };
         };
       };
     };
