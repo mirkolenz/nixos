51c39adfd8215c6f4295d45c04519a964b05ab9e
Mirko Lenz <mirko@mirkolenz.com>
2026-02-16 10:34:28 +0100

    chore(zellij): install everywhere

--- home/mlenz/common/programs/zellij.nix
+++ home/mlenz/common/programs/zellij.nix
@@ -1,22 +1,30 @@
-{ pkgs, osConfig, ... }:
 {
-
-  programs.zellij = {
-    enable = pkgs.stdenv.isLinux && (osConfig.services.openssh.enable or true);
-    enableBashIntegration = true;
-    enableFishIntegration = true;
-    enableZshIntegration = true;
-    attachExistingSession = true; # requires shell integration
-    exitShellOnExit = true; # requires shell integration
-    settings = {
-      auto_layout = true;
-      default_layout = "default";
-      default_mode = "normal";
-      on_force_close = "detach";
-      session_serialization = false;
-      show_release_notes = false;
-      show_startup_tips = false;
-      theme = "default";
-    };
-  };
+  pkgs,
+  osConfig,
+  lib,
+  ...
+}:
+{
+  programs.zellij = lib.mkMerge [
+    {
+      enable = true;
+      settings = {
+        auto_layout = true;
+        default_layout = "default";
+        default_mode = "normal";
+        on_force_close = "detach";
+        session_serialization = false;
+        show_release_notes = false;
+        show_startup_tips = false;
+        theme = "default";
+      };
+    }
+    (lib.mkIf (pkgs.stdenv.isLinux && (osConfig.services.openssh.enable or true)) {
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableZshIntegration = true;
+      attachExistingSession = true; # requires shell integration
+      exitShellOnExit = true; # requires shell integration
+    })
+  ];
 }
