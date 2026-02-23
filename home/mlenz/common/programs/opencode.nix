efc59c0a4a950cb1e4408461731fb971228d3cae
Mirko Lenz <mirko@mirkolenz.com>
2026-02-23 11:23:18 +0100

    chore(opencode): add providers

--- home/mlenz/common/programs/opencode.nix
+++ home/mlenz/common/programs/opencode.nix
@@ -12,6 +12,28 @@ lib.mkIf config.custom.profile.isWorkstation {
     settings = {
       share = "disabled";
       autoupdate = false;
+      model = "lmstudio/openai/gpt-oss-20b";
+      provider = {
+        lmstudio = {
+          npm = "@ai-sdk/openai-compatible";
+          name = "LM Studio";
+          options.baseURL = "http://127.0.0.1:1234/v1";
+          models = {
+            "openai/gpt-oss-20b" = {
+              name = "GPT-OSS";
+            };
+            "qwen/qwen3-32b" = {
+              name = "Qwen3";
+            };
+          };
+        };
+        ollama = {
+          npm = "@ai-sdk/openai-compatible";
+          name = "Ollama";
+          options.baseURL = "http://127.0.0.1:11434/v1";
+          models = { };
+        };
+      };
     };
   };
   home.sessionVariables = {
