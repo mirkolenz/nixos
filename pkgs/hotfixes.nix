8ed029e5fcface8261a45c88f349dbeccd563396
Mirko Lenz <mirko@mirkolenz.com>
2026-02-25 07:56:15 +0100

    chore(deps): update hotfixes

--- pkgs/hotfixes.nix
+++ pkgs/hotfixes.nix
@@ -4,4 +4,8 @@ final: prev:
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin {
+  # https://hydra.nixos.org/job/nixpkgs/unstable/yt-dlp.aarch64-darwin
+  inherit (final.stable)
+    yt-dlp
+    ;
 })
