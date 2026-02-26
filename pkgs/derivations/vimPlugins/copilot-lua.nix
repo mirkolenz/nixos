8a4ac6ccc4e96b0db501ff27841978c73dab8c93
Mirko Lenz <mirko@mirkolenz.com>
2026-02-26 14:06:06 +0100

    chore(deps): update pkgs

--- pkgs/derivations/vimPlugins/copilot-lua.nix
+++ pkgs/derivations/vimPlugins/copilot-lua.nix
@@ -5,12 +5,12 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "copilot.lua";
-  version = "0-unstable-2026-02-24";
+  version = "0-unstable-2026-02-26";
   src = fetchFromGitHub {
     owner = "zbirenbaum";
     repo = "copilot.lua";
-    rev = "ef4bfc579b404c531c1738bacca82ed61501d877";
-    hash = "sha256-2z/NsrZuZqsb9wSMhQA1mMjoLCOU98MqM/s2G5/BNaw=";
+    rev = "9f91e57dc03f38b248789dcc402d27819cc947ed";
+    hash = "sha256-tQ/RfIPY0/2WoZOQdFnOG4V4xsmqg8+ZLFp/PTeyWoM=";
   };
   meta.homepage = "https://github.com/zbirenbaum/copilot.lua/";
   passthru.updateScript = nix-update-script {
