ee7963345e8f3e5c50e86a85e1ae937cc1027741
Mirko Lenz <mirko@mirkolenz.com>
2026-03-01 10:45:55 +0100

    fix(home/zellij): always enable

--- home/mlenz/common/programs/zellij.nix
+++ home/mlenz/common/programs/zellij.nix
@@ -1,29 +1,21 @@
+{ ... }:
 {
-  config,
-  lib,
-  ...
-}:
-{
-  programs.zellij = lib.mkMerge [
-    {
-      enable = true;
-      settings = {
-        auto_layout = true;
-        default_layout = "default";
-        default_mode = "normal";
-        on_force_close = "detach";
-        session_serialization = false;
-        show_release_notes = false;
-        show_startup_tips = false;
-        theme = "default";
-      };
-    }
-    (lib.mkIf config.custom.profile.isServer {
-      enableBashIntegration = true;
-      enableFishIntegration = true;
-      enableZshIntegration = true;
-      attachExistingSession = true; # requires shell integration
-      exitShellOnExit = true; # requires shell integration
-    })
-  ];
+  programs.zellij = {
+    enable = true;
+    settings = {
+      auto_layout = true;
+      default_layout = "default";
+      default_mode = "normal";
+      on_force_close = "quit";
+      session_serialization = false;
+      show_release_notes = false;
+      show_startup_tips = false;
+      theme = "default";
+    };
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
+    attachExistingSession = false;
+    exitShellOnExit = true;
+  };
 }
