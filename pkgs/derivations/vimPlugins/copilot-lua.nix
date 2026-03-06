aa7bf2e8ac15a4cc9ce7ee71f327a18fca5174ac
Mirko Lenz <mirko@mirkolenz.com>
2026-03-06 09:12:00 +0100

    chore(pkgs/vim): switch copilot lua to versioned tags

--- pkgs/derivations/vimPlugins/copilot-lua.nix
+++ pkgs/derivations/vimPlugins/copilot-lua.nix
@@ -3,17 +3,15 @@
   fetchFromGitHub,
   nix-update-script,
 }:
-vimUtils.buildVimPlugin {
+vimUtils.buildVimPlugin rec {
   pname = "copilot.lua";
-  version = "2.0.0-unstable-2026-03-06";
+  version = "2.0.0";
   src = fetchFromGitHub {
     owner = "zbirenbaum";
     repo = "copilot.lua";
-    rev = "437b18f7db78a0ec2bea05dde37791019913e547";
+    rev = "v${version}";
     hash = "sha256-ix+dR/sho4a1f4EliUMWkB396NbPLb7C9t+jN8fiviA=";
   };
   meta.homepage = "https://github.com/zbirenbaum/copilot.lua/";
-  passthru.updateScript = nix-update-script {
-    extraArgs = [ "--version=branch" ];
-  };
+  passthru.updateScript = nix-update-script { };
 }
