638c24cc177768786e1a7949487b7ad9f94fdccd
Mirko Lenz <mirko@mirkolenz.com>
2026-03-07 10:35:06 +0100

    feat(home): add helix

---
+++ home/mlenz/common/programs/helix.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  programs.helix = {
+    enable = true;
+    defaultEditor = false;
+    # https://docs.helix-editor.com/languages.html
+    languages = { };
+    # https://docs.helix-editor.com/configuration.html
+    settings = { };
+  };
+}
