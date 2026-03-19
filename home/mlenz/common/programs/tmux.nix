8a9e93f19effe4306f4c4ad1d2bd81e8f2da5b22
Mirko Lenz <mirko@mirkolenz.com>
2026-03-19 10:10:24 +0100

    feat(home): enable tmux

--- home/mlenz/common/programs/tmux.nix
+++ home/mlenz/common/programs/tmux.nix
@@ -1,17 +1,39 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }:
 {
   programs.tmux = {
-    enable = false;
+    enable = true;
+    # https://github.com/tmux-plugins/tmux-sensible
+    extraConfig = ''
+      set -g display-time 4000
+      set -g status-interval 5
+      set -g status-keys emacs
+      bind C-p previous-window
+      bind C-n next-window
+      bind a last-window
+    '';
+    # keep-sorted start
+    aggressiveResize = true;
     clock24 = true;
+    escapeTime = 0;
+    focusEvents = true;
+    historyLimit = 50000;
     keyMode = "vi";
     mouse = true;
-    newSession = true;
+    newSession = false;
     shell = lib.getExe pkgs.fish;
     shortcut = "a";
-    terminal = "xterm-256color";
+    terminal = "screen-256color";
+    # keep-sorted end
+    plugins = with pkgs.tmuxPlugins; [
+      tmux-powerline
+    ];
+  };
+  home.shellAliases = {
+    t = lib.getExe config.programs.tmux.package;
   };
 }
