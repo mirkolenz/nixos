bd930fcf4638b75d0288d2e86ce8e2b5df8da6df
Mirko Lenz <mirko@mirkolenz.com>
2026-03-30 06:59:00 +0200

    chore(pkgs): update hotfixes

--- pkgs/hotfixes.nix
+++ pkgs/hotfixes.nix
@@ -3,8 +3,4 @@ final: prev:
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin {
-  inherit (final.stable)
-    # https://hydra.nixos.org/job/nixpkgs/unstable/direnv.aarch64-darwin
-    direnv
-    ;
 })
