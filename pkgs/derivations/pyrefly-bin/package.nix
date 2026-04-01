dc8071f8a3c320139fe1a2b3aea02b613cccf9c4
Mirko Lenz <mirko@mirkolenz.com>
2026-04-01 07:22:32 +0200

    chore(pkgs/pyrefly): disable updates

--- pkgs/derivations/pyrefly-bin/package.nix
+++ pkgs/derivations/pyrefly-bin/package.nix
@@ -20,6 +20,9 @@ mkGitHubBinary {
   nativeInstallCheckInputs = [ versionCheckHook ];
   doInstallCheck = true;
 
+  # todo: assets missing in https://github.com/facebook/pyrefly/releases/tag/0.59.0
+  passthru.updateScript = null;
+
   meta = {
     description = "Fast type checker and language server for Python";
     license = lib.licenses.mit;
