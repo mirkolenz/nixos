1258d6e05a02b0d1197d795843c6667f69a7b0ba
Mirko Lenz <mirko@mirkolenz.com>
2026-09-07 22:27:09 +0200

    chore(pkgs/hotfixes): update

--- pkgs/overrides/hotfixes.nix
+++ pkgs/overrides/hotfixes.nix
@@ -3,13 +3,4 @@ final: prev:
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin {
-  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-    (pyFinal: pyPrev: {
-      # Interactive Zsh completion tests capture unreliable terminal output on Darwin.
-      cyclopts = pyPrev.cyclopts.overridePythonAttrs (prevAttrs: {
-        disabledTestPaths = (prevAttrs.disabledTestPaths or [ ]) ++ [ "tests/completion/test_zsh.py" ];
-        disabledTests = (prevAttrs.disabledTests or [ ]) ++ [ "test_behavior[zsh-" ];
-      });
-    })
-  ];
 })
