7bdd72d8ef5f6bcc8065736505abfa47bc383451
Mirko Lenz <mirko@mirkolenz.com>
2026-09-08 07:57:10 +0200

    chore(deps/pkgs): update
    Package updates:
    
    - ty-bin: 0.0.78 -> 0.0.79
    - vimPlugins-copilot-lua: 3.1.1 -> 3.1.3

--- pkgs/by-name/vimPlugins/copilot-lua.nix
+++ pkgs/by-name/vimPlugins/copilot-lua.nix
@@ -5,12 +5,12 @@
 }:
 vimUtils.buildVimPlugin rec {
   pname = "copilot.lua";
-  version = "3.1.1";
+  version = "3.1.3";
   src = fetchFromGitHub {
     owner = "zbirenbaum";
     repo = "copilot.lua";
     rev = "v${version}";
-    hash = "sha256-r98ELUxUFSGEtDephWSLuWjBYwBUzGueFRQBCIeFnrY=";
+    hash = "sha256-FUEUa43dAORuPreCyj+WXZNOEz34bYL/w1ZUFmp1eBA=";
   };
   meta.homepage = "https://github.com/zbirenbaum/copilot.lua/";
   passthru.updateScript = nix-update-script { };
