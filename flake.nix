9b725d276e4d0af41636c9285ca335544dceb1f3
Mirko Lenz <mirko@mirkolenz.com>
2026-09-09 21:50:57 +0200

    chore(deps/nix): update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/a2dbe7c' (2026-09-09)
      → 'github:nix-community/home-manager/8e89a23' (2026-09-09)
    • Updated input 'mistral-vibe':
        'github:mistralai/mistral-vibe/6c79ef0' (2026-09-04)
      → 'github:mistralai/mistral-vibe/2817f3d' (2026-09-09)
    • Updated input 'nixpkgs':
        'https://releases.nixos.org/nixpkgs/nixpkgs-26.11pre1069909.da39501c8d0a/nixexprs.tar.zst' (2026-09-08)
      → 'https://releases.nixos.org/nixpkgs/nixpkgs-26.11pre1070276.a391f95d4557/nixexprs.tar.zst' (2026-09-09)
    • Updated input 'nixpkgs-darwin-stable':
        'https://releases.nixos.org/nixpkgs/26.05-darwin/nixpkgs-darwin-26.05pre1013714.555cb0f648dd/nixexprs.tar.zst' (2026-09-08)
      → 'https://releases.nixos.org/nixpkgs/26.05-darwin/nixpkgs-darwin-26.05pre1013766.104a7c61006c/nixexprs.tar.zst' (2026-09-09)

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
     };
     import-tree.url = "github:denful/import-tree";
     mistral-vibe = {
-      url = "github:mistralai/mistral-vibe/v2.25.0";
+      url = "github:mistralai/mistral-vibe/v2.25.1";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         pyproject-nix.follows = "pyproject-nix";
